baa3de111fd4d5dbe6b4a2c47be997a1213747b6
Seb Jones <seb.jones@enovate.co.uk>
2023-07-05 20:50:26 +0100

    Various style tweaks

--- app/components/charts/TopTenFundersByTotalAwardedDoughnutChart.tsx
+++ app/components/charts/TopTenFundersByTotalAwardedDoughnutChart.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
+import { Chart as ChartJS, ArcElement, Legend, Tooltip } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 import data from '../../../data/dist/charts/topTenFundersByTotalAwarded.json'
 
-ChartJS.register(ArcElement, Tooltip);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function TopTenFundersByTotalAwardedDoughnutChart() {
     return <Doughnut data={data} />
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,13 @@ import TopTenFundersByTotalAwardedDoughnutChart from './components/charts/TopTen
 export default function Home() {
     return (
         <div className="p-16">
-            <div className="container mx-auto grid grid-cols-3">
+            <div className="container mx-auto grid grid-cols-2">
                 <div className="col-span-1 flex flex-col gap-y-4">
                     <h1 className="text-2xl font-bold text-center">
                         Top Ten Funders by Total Awarded
                     </h1>
 
                     <TopTenFundersByTotalAwardedDoughnutChart />
-                </div>
-
-                <div className="col-span-1">
                 </div>
 
                 <div className="col-span-1">
