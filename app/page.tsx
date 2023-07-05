59570ba8cb7b2830ffb78b60537ea330c0f0e233
Seb Jones <seb.jones@enovate.co.uk>
2023-07-05 20:40:27 +0100

    Format

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
         <div className="p-16">
             <div className="container mx-auto grid grid-cols-3">
                 <div className="col-span-1 flex flex-col gap-y-4">
-                    <h1 className="text-2xl font-bold text-center">Top Ten Funders by Total Awarded</h1>
+                    <h1 className="text-2xl font-bold text-center">
+                        Top Ten Funders by Total Awarded
+                    </h1>
+
                     <TopTenFundersByTotalAwardedDoughnutChart />
                 </div>
 
