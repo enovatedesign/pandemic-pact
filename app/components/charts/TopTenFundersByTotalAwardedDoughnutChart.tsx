a582060e9fb668e50d1f7763f98e7601cfce25fc
Seb Jones <seb.jones@enovate.co.uk>
2023-07-06 19:31:51 +0100

    Millify doughnut label

--- app/components/charts/TopTenFundersByTotalAwardedDoughnutChart.tsx
+++ app/components/charts/TopTenFundersByTotalAwardedDoughnutChart.tsx
@@ -1,12 +1,24 @@
 "use client"
 
-import { Chart as ChartJS, ArcElement, Legend, Tooltip } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import {Chart as ChartJS, ArcElement, Legend, Tooltip, type TooltipItem} from "chart.js";
+import {Doughnut} from "react-chartjs-2";
+import {millify} from "millify";
 
 import data from '../../../data/dist/charts/topTenFundersByTotalAwarded.json'
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+const options = {
+    plugins: {
+        tooltip: {
+            callbacks: {
+                label: (context: TooltipItem<"doughnut">) => '$' + millify(context.parsed, {precision: 2}),
+            },
+        },
+    },
+};
+
+
 export default function TopTenFundersByTotalAwardedDoughnutChart() {
-    return <Doughnut data={data} />
+    return <Doughnut data={data} options={options} />
 }
