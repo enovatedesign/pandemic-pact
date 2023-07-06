08f7061f8c564fa59034d9660de0f3e34b85525f
Seb Jones <seb.jones@enovate.co.uk>
2023-07-06 19:41:34 +0100

    Use ChartJS built-in title

--- app/components/charts/TopTenFundersByTotalAwardedDoughnutChart.tsx
+++ app/components/charts/TopTenFundersByTotalAwardedDoughnutChart.tsx
@@ -1,15 +1,23 @@
 "use client"
 
-import {Chart as ChartJS, ArcElement, Legend, Tooltip, type TooltipItem} from "chart.js";
+import {Chart as ChartJS, ArcElement, Legend, Title, Tooltip, type TooltipItem} from "chart.js";
 import {Doughnut} from "react-chartjs-2";
 import {millify} from "millify";
 
 import data from '../../../data/dist/charts/topTenFundersByTotalAwarded.json'
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Title, Tooltip, Legend);
 
 const options = {
     plugins: {
+        title: {
+            display: true,
+            text: 'Top Ten Funders by Total Awarded',
+            font: {
+                size: 24,
+                weight: 'bold',
+            },
+        },
         tooltip: {
             callbacks: {
                 label: (context: TooltipItem<"doughnut">) => '$' + millify(context.parsed, {precision: 2}),
