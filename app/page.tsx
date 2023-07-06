03458df1c74c4bc5c8951d54ef91589db6a82d56
Seb Jones <seb.jones@enovate.co.uk>
2023-07-06 19:36:49 +0100

    Grid gap

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TopTenFundersByTotalAwardedDoughnutChart from './components/charts/TopTen
 export default function Home() {
     return (
         <div className="p-16">
-            <div className="container mx-auto grid grid-cols-2">
+            <div className="container mx-auto grid grid-cols-2 gap-4">
                 <div className="col-span-1 flex flex-col gap-y-4">
                     <h1 className="text-2xl font-bold text-center">
                         Top Ten Funders by Total Awarded
