f9a08c9c274924005166ec3a2f86f4c8b6cee8c5
Seb Jones <seb.jones@enovate.co.uk>
2023-08-18 11:03:04 +0100

    Basic fake-data copy of 'Funded COVID-19 Research Projects by WHO Research Priorities'

---
+++ app/WhoRoadmapResearchPrioritiesCard.tsx
@@ -0,0 +1,169 @@
+import {BarList, Card, Title, List, ListItem, Grid, Col} from "@tremor/react"
+
+const whoResearchPriorities = [
+    {
+        value: '1',
+        name: "Virus: natural history, transmission and diagnostics",
+    },
+    {
+        value: '2',
+        name: "Animal and environmental research",
+    },
+    {
+        value: '3',
+        name: "Epidemiological studies",
+    },
+    {
+        value: '4',
+        name: "Clinical characterization and management",
+    },
+    {
+        value: '5',
+        name: "Infection prevention and control",
+    },
+    {
+        value: '6',
+        name: "Candidate therapeutics R&D",
+    },
+    {
+        value: '7',
+        name: "Candidate vaccines R&D",
+    },
+    {
+        value: '8',
+        name: "Ethics considerations for research",
+    },
+    {
+        value: '9',
+        name: "Social sciences in the outbreak response",
+    },
+    {
+        value: '',
+        name: "N/A",
+    },
+]
+
+const numberOfGrantsPerWhoResearchPriority = [
+    {
+        value: 456,
+        name: '',
+    },
+    {
+        value: 351,
+        name: '',
+    },
+    {
+        value: 271,
+        name: '',
+    },
+    {
+        value: 191,
+        name: '',
+    },
+    {
+        value: 91,
+        name: '',
+    },
+    {
+        value: 456,
+        name: '',
+    },
+    {
+        value: 351,
+        name: '',
+    },
+    {
+        value: 271,
+        name: '',
+    },
+    {
+        value: 191,
+        name: '',
+    },
+    {
+        value: 91,
+        name: '',
+    },
+]
+
+const amountOfMoneySpentPerWhoResearchPriority = [
+    {
+        value: 456,
+        name: '',
+    },
+    {
+        value: 351,
+        name: '',
+    },
+    {
+        value: 271,
+        name: '',
+    },
+    {
+        value: 191,
+        name: '',
+    },
+    {
+        value: 91,
+        name: '',
+    },
+    {
+        value: 456,
+        name: '',
+    },
+    {
+        value: 351,
+        name: '',
+    },
+    {
+        value: 271,
+        name: '',
+    },
+    {
+        value: 191,
+        name: '',
+    },
+    {
+        value: 91,
+        name: '',
+    },
+]
+
+const amountOfMoneySpentPerWhoResearchPriorityValueFormatter = (value: number) => {
+    return `$${value}M`
+}
+
+export default function WhoRoadmapResearchPrioritiesCard() {
+    return (
+        <Card>
+            <Title>Funded COVID-19 Research Projects by WHO Research Priorities</Title>
+
+            <Grid className="mt-6 gap-12" numItems={3}>
+                <Col>
+                    <List>
+                        {whoResearchPriorities.map((item) => (
+                            <ListItem
+                                key={item.value}
+                                className="h-9 mb-2 border-none justify-start"
+                            >
+                                <span className="w-8">{item.value}</span>
+                                <span>{item.name}</span>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Col>
+
+                <Col>
+                    <BarList data={numberOfGrantsPerWhoResearchPriority} />
+                </Col>
+
+                <Col>
+                    <BarList
+                        data={amountOfMoneySpentPerWhoResearchPriority}
+                        valueFormatter={amountOfMoneySpentPerWhoResearchPriorityValueFormatter}
+                    />
+                </Col>
+            </Grid>
+        </Card>
+    )
+}
