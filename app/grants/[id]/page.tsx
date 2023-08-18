eac1ac41444634a7fc45f903d77df28a420d7b70
Seb Jones <seb.jones@enovate.co.uk>
2023-08-18 14:15:22 +0100

    Output some basic cards on grant detail page

--- app/grants/[id]/page.tsx
+++ app/grants/[id]/page.tsx
@@ -1,15 +1,53 @@
-// TODO only import the specific grant identified by `id` from the dataset if possible
+// TODO only import the specific grant identified by `id` from the dataset if possible?
 
+import {Grid, Col, Card, Title, Subtitle, Flex, Icon, Text, Metric} from '@tremor/react'
 import completeDataset from '../../../data/dist/complete-dataset.json'
-
-export default function Page({params}: {params: {id: string}}) {
-    const grant = completeDataset.find((grant: {GrantID: number}) => grant.GrantID === parseInt(params.id))
-
-    return <div>My Grant: {grant.GrantID}</div>
-}
 
 export async function generateStaticParams() {
     return completeDataset.map((grant: {GrantID: number}) => ({
         id: `${grant.GrantID}`,
     }))
+}
+
+export default function Page({params}: {params: {id: string}}) {
+    const grant = completeDataset.find((grant: {GrantID: number}) => grant.GrantID === parseInt(params.id))
+
+    const sidebarItems = [
+        {
+            text: 'Amount Awarded (USD)',
+            metric: `$ ${grant.GrantAmountConverted.toFixed(2)}`,
+        },
+        {
+            text: 'End Year',
+            metric: grant.GrantEndYear,
+        },
+    ]
+
+    return (
+        <Grid numItemsLg={6} className="gap-6 mt-6">
+            <Col numColSpanLg={4}>
+                <Card className="h-full">
+                    <Title>{grant.GrantTitleEng}</Title>
+
+                    <Subtitle className="mt-4">Abstract</Subtitle>
+                    <Text className="mt-2">{grant.Abstract}</Text>
+                </Card>
+            </Col>
+
+            <Col numColSpanLg={2}>
+                <div className="space-y-6">
+                    {sidebarItems.map(({text, metric}, index) => (
+                        <Card key={index}>
+                            <Flex justifyContent="start" className="space-x-4">
+                                <div className="truncate">
+                                    <Text>{text}</Text>
+                                    <Metric className="truncate mt-2">{metric}</Metric>
+                                </div>
+                            </Flex>
+                        </Card>
+                    ))}
+                </div>
+            </Col>
+        </Grid>
+    )
 }
