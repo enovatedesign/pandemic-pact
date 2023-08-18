64ee26aa76406cb83f1eef270fa8461f5615fbba
Seb Jones <seb.jones@enovate.co.uk>
2023-08-18 10:15:34 +0100

    Move outer shell code into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,28 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import {Title, Text} from '@tremor/react'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: 'Pandemic PACT',
-  description: '',
+    title: 'Pandemic PACT',
+    description: '',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <main className="container mx-auto px-12 py-12">
+                    <Title>Pandemic PACT Tracker</Title>
+                    <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+                    <div className="mt-6">{children}</div>
+                </main>
+            </body>
+        </html>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,50 @@
 import {
     Card,
     Grid,
-    Title,
-    Text,
     Tab,
     TabList,
     TabGroup,
     TabPanel,
     TabPanels,
-} from "@tremor/react";
+} from "@tremor/react"
 
 export default function Home() {
     return (
-        <main className="container mx-auto px-12 py-12">
-            <Title>Pandemic PACT Tracker</Title>
-            <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
-
-            <TabGroup className="mt-6">
-                <TabList>
-                    <Tab>Visualise</Tab>
-                    <Tab>Explore</Tab>
-                </TabList>
-                <TabPanels>
-                    <TabPanel>
-                        <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
-                            <Card>
-                                {/* Placeholder to set height */}
-                                <div className="h-28" />
-                            </Card>
-                            <Card>
-                                {/* Placeholder to set height */}
-                                <div className="h-28" />
-                            </Card>
-                            <Card>
-                                {/* Placeholder to set height */}
-                                <div className="h-28" />
-                            </Card>
-                        </Grid>
-                        <div className="mt-6">
-                            <Card>
-                                <div className="h-80" />
-                            </Card>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div className="mt-6">
-                            <Card>
-                                <div className="h-96" />
-                            </Card>
-                        </div>
-                    </TabPanel>
-                </TabPanels>
-            </TabGroup>
-        </main>
+        <TabGroup className="mt-6">
+            <TabList>
+                <Tab>Visualise</Tab>
+                <Tab>Explore</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
+                        <Card>
+                            {/* Placeholder to set height */}
+                            <div className="h-28" />
+                        </Card>
+                        <Card>
+                            {/* Placeholder to set height */}
+                            <div className="h-28" />
+                        </Card>
+                        <Card>
+                            {/* Placeholder to set height */}
+                            <div className="h-28" />
+                        </Card>
+                    </Grid>
+                    <div className="mt-6">
+                        <Card>
+                            <div className="h-80" />
+                        </Card>
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className="mt-6">
+                        <Card>
+                            <div className="h-96" />
+                        </Card>
+                    </div>
+                </TabPanel>
+            </TabPanels>
+        </TabGroup>
     );
 }
