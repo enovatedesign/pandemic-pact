5662c09988daccc101d105243c83b75f4608c144
Seb Jones <seb.jones@enovate.co.uk>
2023-08-20 11:31:01 +0100

    Implement AmountSpentOnEachResearchCategoryOverTimeCard bar chart

--- app/AmountSpentOnEachResearchCategoryOverTimeCard.tsx
+++ app/AmountSpentOnEachResearchCategoryOverTimeCard.tsx
@@ -1,14 +1,15 @@
 import {useState} from "react"
-import {Flex, Button, BarList, Card, Title, Subtitle, List, ListItem, Grid, Col, MultiSelect, MultiSelectItem, Text} from "@tremor/react"
+import {Flex, Button, BarChart, Card, Title, Subtitle, List, ListItem, Grid, Col, MultiSelect, MultiSelectItem, Text} from "@tremor/react"
 import {DownloadIcon} from "@heroicons/react/solid"
 import {type StringDictionary} from "../scripts/types/dictionary"
 import {millify} from "millify"
 import meilisearchRequest from './helpers/meilisearch-request'
 import exportToXlsx from "./helpers/export-to-xlsx"
+import {groupBy, mapValues} from 'lodash'
 
 import funders from '../data/source/funders.json'
 import lookupTables from '../data/source/lookup-tables.json'
-import dataset from '../data/dist/grants-by-research-category-card.json'
+import dataset from '../data/dist/amount-spent-on-each-research-category-over-time-card.json'
 
 export default function AmountSpentOnEachResearchCategoryOverTimeCard() {
     const [selectedFunders, setSelectedFunders] = useState<string[]>([])
@@ -16,40 +17,48 @@ export default function AmountSpentOnEachResearchCategoryOverTimeCard() {
 
     const researchCatLookupTable = lookupTables.ResearchCat as StringDictionary
 
-    const researchCategories: {value: string, name: string}[] = Object.keys(researchCatLookupTable).map((key: string) => ({
-        value: key,
-        name: researchCatLookupTable[key],
-    }))
+    const researchCategories: string[] = Object.values(researchCatLookupTable)
 
     const filteredDataset = selectedFunders.length > 0
         ? dataset.filter(grant => selectedFunders.includes(grant.FundingOrgName))
         : dataset
 
-    const numberOfGrantsPerResearchCategory = researchCategories.map(function (researchCategory) {
-        const value = filteredDataset
-            .filter(grant => grant.ResearchCat === researchCategory.name)
-            .length
+    const datasetGroupedByYear = groupBy(filteredDataset, 'GrantEndYear')
 
-        return {
-            key: `grants-per-category-${researchCategory.value}`,
-            value: value,
-            name: '',
-        }
+    const amountSpentOnEachResearchCategoryOverTime = Object.keys(
+        datasetGroupedByYear
+    ).map(year => {
+        const grants = datasetGroupedByYear[year]
+
+        let datapoint: {[key: string]: string | number} = {year}
+
+        researchCategories.forEach(researchCategory => {
+            datapoint[researchCategory] = grants
+                .filter(grant => grant.ResearchCat === researchCategory)
+                .reduce((sum, grant) => sum + grant.GrantAmountConverted, 0)
+        })
+
+        return datapoint
     })
 
-    const amountOfMoneySpentPerResearchCategory = researchCategories.map(function (researchCategory) {
-        const value = filteredDataset
-            .filter(grant => grant.ResearchCat === researchCategory.name)
-            .reduce((sum, grant) => sum + grant.GrantAmountConverted, 0)
+    const colours = [
+        'red',
+        'lime',
+        'cyan',
+        'violet',
+        'orange',
+        'emerald',
+        'indigo',
+        'purple',
+        'amber',
+        'green',
+        'blue',
+        'fuchsia',
+        'yellow',
+        'neutral',
+    ]
 
-        return {
-            key: `grant-amount-${researchCategory.value}`,
-            value: value,
-            name: '',
-        }
-    })
-
-    const amountOfMoneySpentPerResearchCategoryValueFormatter = (value: number) => {
+    const valueFormatter = (value: number) => {
         return '$' + millify(value, {precision: 2})
     }
 
@@ -72,91 +81,63 @@ export default function AmountSpentOnEachResearchCategoryOverTimeCard() {
     }
 
     return (
-        <Card>
+        <Card
+            className="flex flex-col gap-y-6"
+        >
             <Flex
-                flexDirection="col"
-                alignItems="start"
-                className="gap-y-6"
+                justifyContent="between"
+                alignItems="center"
             >
-                <Flex
-                    justifyContent="between"
-                    alignItems="center"
+                <Title>Amount Spent On Each Research Category Over Time</Title>
+                <Text>Total Grants: {dataset.length}</Text>
+            </Flex>
+
+            <Flex
+                justifyContent="between"
+                alignItems="center"
+            >
+                <MultiSelect
+                    value={selectedFunders}
+                    onValueChange={setSelectedFunders}
+                    placeholder="Select funders..."
+                    className="max-w-xs"
                 >
-                    <Title>Amount Spent On Each Research Category Over Time</Title>
-                    <Text>Total Grants: {dataset.length}</Text>
-                </Flex>
+                    {funders.map((funderName) => (
+                        <MultiSelectItem key={funderName} value={funderName}>
+                            {funderName}
+                        </MultiSelectItem>
+                    ))}
+                </MultiSelect>
 
-                <Flex
-                    justifyContent="between"
-                    alignItems="center"
+                {selectedFunders.length > 0 &&
+                    <Text>Filtered Grants: {filteredDataset.length}</Text>
+                }
+            </Flex>
+
+            <div>
+                <BarChart
+                    data={amountSpentOnEachResearchCategoryOverTime}
+                    index="year"
+                    categories={researchCategories}
+                    valueFormatter={valueFormatter}
+                    colors={colours}
+                    showLegend={false}
+                    className="h-96"
+                />
+            </div>
+
+            <Flex
+                justifyContent="end"
+            >
+
+                <Button
+                    icon={DownloadIcon}
+                    loading={exportingResults}
+                    disabled={exportingResults}
+                    onClick={exportResults}
                 >
-                    <MultiSelect
-                        value={selectedFunders}
-                        onValueChange={setSelectedFunders}
-                        placeholder="Select funders..."
-                        className="max-w-xs"
-                    >
-                        {funders.map((funderName) => (
-                            <MultiSelectItem key={funderName} value={funderName}>
-                                {funderName}
-                            </MultiSelectItem>
-                        ))}
-                    </MultiSelect>
-
-                    {selectedFunders.length > 0 &&
-                        <Text>Filtered Grants: {filteredDataset.length}</Text>
-                    }
-                </Flex>
-
-                <Grid
-                    className="gap-12"
-                    numItems={3}
-                >
-                    <Col>
-                        <List>
-                            {researchCategories.map((item) => (
-                                <ListItem
-                                    key={item.value}
-                                    className="h-9 mb-2 border-none justify-start"
-                                >
-                                    <span className="min-w-[2rem]">{item.value}</span>
-                                    <span className="truncate">{item.name}</span>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Col>
-
-                    <Col>
-                        <BarList
-                            data={numberOfGrantsPerResearchCategory}
-                        />
-
-                        <Subtitle className="mt-4 text-right">Number of projects</Subtitle>
-                    </Col>
-
-                    <Col>
-                        <BarList
-                            data={amountOfMoneySpentPerResearchCategory}
-                            valueFormatter={amountOfMoneySpentPerResearchCategoryValueFormatter}
-                        />
-
-                        <Subtitle className="mt-4 text-right">Known value of projects (USD)</Subtitle>
-                    </Col>
-                </Grid>
-
-                <Flex
-                    justifyContent="end"
-                >
-
-                    <Button
-                        icon={DownloadIcon}
-                        loading={exportingResults}
-                        disabled={exportingResults}
-                        onClick={exportResults}
-                    >
-                        Export Results To XLSX
-                    </Button>
-                </Flex>
+                    Export Results To XLSX
+                </Button>
             </Flex>
         </Card>
     )
