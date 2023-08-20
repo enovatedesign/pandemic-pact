abeebb140d3dd44e4a2bab0f8ed90cf6083adfb4
Seb Jones <seb.jones@enovate.co.uk>
2023-08-20 10:23:04 +0100

    Boilerplate a new card for AmountSpentOnEachResearchCategoryOverTime

---
+++ app/AmountSpentOnEachResearchCategoryOverTimeCard.tsx
@@ -0,0 +1,133 @@
+import {useState} from "react"
+import {Flex, Button, BarList, Card, Title, List, ListItem, Grid, Col, MultiSelect, MultiSelectItem} from "@tremor/react"
+import {DownloadIcon} from "@heroicons/react/solid"
+import {type StringDictionary} from "../scripts/types/dictionary"
+import {millify} from "millify"
+import meilisearchRequest from './helpers/meilisearch-request'
+import exportToXlsx from "./helpers/export-to-xlsx"
+
+import funders from '../data/source/funders.json'
+import lookupTables from '../data/source/lookup-tables.json'
+import dataset from '../data/dist/grants-by-research-category-card.json'
+
+export default function AmountSpentOnEachResearchCategoryOverTimeCard() {
+    const [selectedFunders, setSelectedFunders] = useState<string[]>([])
+    const [exportingResults, setExportingResults] = useState<boolean>(false)
+
+    const researchCatLookupTable = lookupTables.ResearchCat as StringDictionary
+
+    const researchCategories: {value: string, name: string}[] = Object.keys(researchCatLookupTable).map((key: string) => ({
+        value: key,
+        name: researchCatLookupTable[key],
+    }))
+
+    const filteredDataset = selectedFunders.length > 0
+        ? dataset.filter(grant => selectedFunders.includes(grant.FundingOrgName))
+        : dataset
+
+    const numberOfGrantsPerResearchCategory = researchCategories.map(function (researchCategory) {
+        const value = filteredDataset
+            .filter(grant => grant.ResearchCat === researchCategory.name)
+            .length
+
+        return {
+            key: `grants-per-category-${researchCategory.value}`,
+            value: value,
+            name: '',
+        }
+    })
+
+    const amountOfMoneySpentPerResearchCategory = researchCategories.map(function (researchCategory) {
+        const value = filteredDataset
+            .filter(grant => grant.ResearchCat === researchCategory.name)
+            .reduce((sum, grant) => sum + grant.GrantAmountConverted, 0)
+
+        return {
+            key: `grant-amount-${researchCategory.value}`,
+            value: value,
+            name: '',
+        }
+    })
+
+    const amountOfMoneySpentPerResearchCategoryValueFormatter = (value: number) => {
+        return '$' + millify(value, {precision: 2})
+    }
+
+    const exportResults = () => {
+        setExportingResults(true)
+
+        const body = {
+            filter: `GrantID IN [${filteredDataset.map(grant => grant.GrantID).join(',')}]`,
+            sort: ['GrantID:asc'],
+            limit: 100_000, // TODO determine this based on number of generated grants in complete dataset?
+        }
+
+        meilisearchRequest('exports', body).then(data => {
+            exportToXlsx('pandemic-pact-grants-by-research-category-export.xlsx', data.hits)
+            setExportingResults(false)
+        }).catch((error) => {
+            console.error('Error:', error)
+            setExportingResults(false)
+        })
+    }
+
+    return (
+        <Card>
+            <Flex
+                justifyContent="between"
+                alignItems="start"
+            >
+                <Title>Amount Spent On Each Research Category Over Time</Title>
+
+                <Button
+                    icon={DownloadIcon}
+                    loading={exportingResults}
+                    disabled={exportingResults}
+                    onClick={exportResults}
+                >
+                    Export Results To XLSX
+                </Button>
+            </Flex>
+
+            <MultiSelect
+                value={selectedFunders}
+                onValueChange={setSelectedFunders}
+                placeholder="Select funders..."
+                className="max-w-xs mt-6"
+            >
+                {funders.map((funderName) => (
+                    <MultiSelectItem key={funderName} value={funderName}>
+                        {funderName}
+                    </MultiSelectItem>
+                ))}
+            </MultiSelect>
+
+            <Grid className="mt-6 gap-12" numItems={3}>
+                <Col>
+                    <List>
+                        {researchCategories.map((item) => (
+                            <ListItem
+                                key={item.value}
+                                className="h-9 mb-2 border-none justify-start"
+                            >
+                                <span className="min-w-[2rem]">{item.value}</span>
+                                <span className="truncate">{item.name}</span>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Col>
+
+                <Col>
+                    <BarList data={numberOfGrantsPerResearchCategory} />
+                </Col>
+
+                <Col>
+                    <BarList
+                        data={amountOfMoneySpentPerResearchCategory}
+                        valueFormatter={amountOfMoneySpentPerResearchCategoryValueFormatter}
+                    />
+                </Col>
+            </Grid>
+        </Card>
+    )
+}
--- app/VisualiseTabPanel.tsx
+++ app/VisualiseTabPanel.tsx
@@ -1,13 +1,22 @@
-import {TabPanel, Grid, Card} from "@tremor/react"
+import {TabPanel, Grid, Col, Card} from "@tremor/react"
 
 import GrantsByResearchCategoryCard from './GrantsByResearchCategoryCard'
+import AmountSpentOnEachResearchCategoryOverTimeCard from './AmountSpentOnEachResearchCategoryOverTimeCard'
 
 export default function VisualiseTabPanel() {
     return (
         <TabPanel>
-            <div className="mt-6">
-                <GrantsByResearchCategoryCard />
-            </div>
+            <Grid
+                numItems={1}
+                className="mt-6 gap-4"
+            >
+                <Col>
+                    <GrantsByResearchCategoryCard />
+                </Col>
+                <Col>
+                    <AmountSpentOnEachResearchCategoryOverTimeCard />
+                </Col>
+            </Grid>
         </TabPanel>
     )
 }
