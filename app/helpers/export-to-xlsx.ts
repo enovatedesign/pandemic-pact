73c7630b5f00b3aa4b7817dfbde2d44f572ea083
Seb Jones <seb.jones@enovate.co.uk>
2023-08-30 16:02:36 +0100

    Don't import entire namespace from XLSX package

--- app/helpers/export-to-xlsx.ts
+++ app/helpers/export-to-xlsx.ts
@@ -1,11 +1,11 @@
-import * as XLSX from 'xlsx'
+import {utils, writeFile} from 'xlsx'
 
 export default async function exportToXlsx(filename: string, hits: any[]) {
-    const worksheet = XLSX.utils.json_to_sheet(hits)
+    const worksheet = utils.json_to_sheet(hits)
 
-    const workbook = XLSX.utils.book_new()
+    const workbook = utils.book_new()
 
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Grants")
+    utils.book_append_sheet(workbook, worksheet, "Grants")
 
-    XLSX.writeFile(workbook, `${filename}.csv`, {bookType: 'csv'})
+    writeFile(workbook, `${filename}.csv`, {bookType: 'csv'})
 }
