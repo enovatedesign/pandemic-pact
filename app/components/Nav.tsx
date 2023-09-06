28599670c88a2ec2206815409edc79564c35ca60
Seb Jones <seb.jones@enovate.co.uk>
2023-09-06 11:19:54 +0100

    Rename explore path to grants

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -20,7 +20,7 @@ export default function Nav({selected}: Props) {
             <Link
                 className="tremor-Tab-root flex whitespace-nowrap truncate max-w-xs outline-none focus:ring-0 text-tremor-default ui-selected:text-tremor-brand dark:ui-selected:text-dark-tremor-brand ui-selected:border-b-2 hover:border-b-2 border-transparent transition duration-100 hover:border-tremor-content hover:text-tremor-content-emphasis text-tremor-content dark:hover:border-dark-tremor-content-emphasis dark:hover:text-dark-tremor-content-emphasis dark:text-dark-tremor-content ui-selected:border-tremor-brand dark:ui-selected:border-dark-tremor-brand -mb-px px-2 py-2" id="headlessui-tabs-tab-:r2a:" role="tab" type="button"
                 data-headlessui-state={selected === 'explore' ? 'selected' : selected}
-                href="/explore"
+                href="/grants"
             >
                 <span>Explore</span>
             </Link>
