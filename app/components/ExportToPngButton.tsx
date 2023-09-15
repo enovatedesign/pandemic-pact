50b616afccdda2e319db204b54b58c4add961100
Seb Jones <seb.jones@enovate.co.uk>
2023-09-15 09:59:02 +0100

    Proof-of-concept for adding licence text to bottom of exported images. Closes #18

--- app/components/ExportToPngButton.tsx
+++ app/components/ExportToPngButton.tsx
@@ -23,7 +23,18 @@ export default function ExportToPngButton({selector, filename}: Props) {
 
         const ignoreElements = (element: Element) => element.classList.contains('ignore-in-image-export')
 
-        html2canvas(element, {ignoreElements}).then(canvas => {
+        const onclone = (document: Document, element: HTMLElement) => {
+            // Add a <p> tag with CCO licence info
+            const p = document.createElement('p')
+            p.style.marginTop = '1rem'
+            p.innerText = 'This image is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.'
+            element.appendChild(p)
+        }
+
+        html2canvas(element, {
+            ignoreElements,
+            onclone,
+        }).then(canvas => {
             const png = canvas.toDataURL('image/png')
 
             // download the png to the user's computer
