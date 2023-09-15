c4729e3aa1b675ee30ff919457797003d4d7d44b
Seb Jones <seb.jones@enovate.co.uk>
2023-09-15 20:16:47 +0100

    Protype basic rotating globe

--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,40 @@
 "use client"
 
-import {useState} from "react"
-import {Grid, Col} from "@tremor/react"
-import Nav from "./components/Nav"
-import Layout from "./components/Layout"
-import FilterSidebar from "./components/FilterSidebar"
-import GrantsByResearchCategoryCard from './components/GrantsByResearchCategoryCard'
-import AmountSpentOnEachResearchCategoryOverTimeCard from './components/AmountSpentOnEachResearchCategoryOverTimeCard'
-import GrantsByRegionCard from './components/GrantsByRegionCard'
-import GrantsByMeshClassificationCard from './components/GrantsByMeshClassificationCard'
-import GrantsByCountryWhereResearchWasConductedCard from './components/GrantsByCountryWhereResearchWasConductedCard'
-import {type Filters} from "./types/filters"
+import {useRef} from "react"
+import {Canvas, useFrame} from '@react-three/fiber'
+import {Sphere, useTexture} from "@react-three/drei"
 
 export default function Home() {
-    const [selectedFilters, setSelectedFilters] = useState<Filters>({
-        FundingOrgName: [],
-        ResearchCat: [],
-        Pathogen: [],
-    })
+    return (
+        <div className="w-screen h-screen">
+            <Canvas
+                camera={{
+                    position: [0, 0.4, 2.5],
+                    rotation: [0.4, 0, 0],
+                    fov: 40,
+                }}
+            >
+                <ambientLight />
+                <Globe />
+            </Canvas>
+        </div>
+    )
+}
+
+function Globe() {
+    const textureProps = useTexture({map: "/earth.jpg"})
+
+    const sphereRef = useRef()
+
+    useFrame((state, delta) => (sphereRef.current.rotation.y += delta * 0.05))
 
     return (
-        <Layout
-            title="Pandemic PACT Tracker"
-            summary="Lorem ipsum dolor sit amet, consetetur sadipscing elitr."
-            sidebarContent={
-                <FilterSidebar
-                    selectedFilters={selectedFilters}
-                    setSelectedFilters={setSelectedFilters}
-                />
-            }
+        <Sphere
+            args={[1, 64, 64]}
+            ref={sphereRef}
+            rotation={[Math.PI, 0, Math.PI]}
         >
-            <div className="mt-6">
-                <Nav selected="visualise" />
-
-                <Grid
-                    numItems={12}
-                    className="mt-6 gap-4"
-                >
-                    <Col numColSpan={12}>
-                        <GrantsByResearchCategoryCard
-                            selectedFilters={selectedFilters}
-                        />
-                    </Col>
-
-                    <Col numColSpan={12}>
-                        <GrantsByCountryWhereResearchWasConductedCard
-                            selectedFilters={selectedFilters}
-                        />
-                    </Col>
-
-                    <Col numColSpan={12}>
-                        <AmountSpentOnEachResearchCategoryOverTimeCard
-                            selectedFilters={selectedFilters}
-                        />
-                    </Col>
-
-                    <Col numColSpan={5}>
-                        <GrantsByRegionCard
-                            selectedFilters={selectedFilters}
-                        />
-                    </Col>
-
-                    <Col numColSpan={7}>
-                        <GrantsByMeshClassificationCard
-                            selectedFilters={selectedFilters}
-                        />
-                    </Col>
-                </Grid>
-            </div>
-        </Layout>
+            <meshStandardMaterial {...textureProps} />
+        </Sphere>
     )
 }
