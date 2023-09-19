b53d9c57633d2b37f3243a9a541c4c2cb8bd7873
Josh Lamb <josh.lamb@enovate.co.uk>
2023-09-19 10:31:56 +0100

    Circles within the dot grid background now follow mouse movement

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,38 @@ interface InteractiveBackgroundProps {
 export function InteractiveBackground({children, ...rest}: InteractiveBackgroundProps) {
     const canvas = useRef<HTMLCanvasElement>(null)
 
-    const drawCircles = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => {
-        canvas.width = canvas.clientWidth
-        canvas.height = canvas.clientHeight
+    const drawCircles = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, mouse?: {x:number, y:number}) => {
+        const width = canvas.clientWidth
+        const height = canvas.clientHeight
 
+        const centerX = width / 2
+        const centerY = height / 2
+        const maxRadius = Math.sqrt(Math.pow(centerX, 2) + Math.pow(centerY, 2))
+
+        canvas.width = width
+        canvas.height = height
         ctx.fillStyle = 'rgba(255, 255, 255, 0.25)'
+
+        if (!mouse) {
+            mouse = {
+                x: centerX,
+                y: centerY,
+            }
+        }
 
         for (let x = 0; x < canvas.width; x += 100) {
             for (let y = 0; y < canvas.height; y += 100) {
+                const distance = Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2))
+                // Increase the radius to make the effect more pronounced
+                const radius = (maxRadius - distance) / maxRadius * 30
+
+                const angle = Math.atan2(mouse.y - y, mouse.x - x)
+                const newX = x + Math.cos(angle) * radius
+                const newY = y + Math.sin(angle) * radius
+
                 ctx.beginPath()
-                ctx.arc(x, y, 3, 0, 2 * Math.PI)
+                ctx.arc(newX, newY, 3, 0, 2 * Math.PI)
+                ctx.closePath()
                 ctx.fill()
             }
         }
@@ -38,6 +60,18 @@ export function InteractiveBackground({children, ...rest}: InteractiveBackground
             ctx.clearRect(0, 0, canvas.width, canvas.height)
             drawCircles(canvas, ctx)
         })
+
+        window.addEventListener('mousemove', event => {
+            const bounds = canvas.getBoundingClientRect();
+
+            const mouse = {
+                x: event.pageX - bounds.left - scrollX,
+                y: event.pageY - bounds.top - scrollY
+            }
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            drawCircles(canvas, ctx, mouse)
+        });
     }
 
     useEffect(() => {
