09aede486e580c303de5d7b61835c3ffed9c6146
Seb Jones <seb.jones@enovate.co.uk>
2023-09-22 11:58:34 +0100

    Move 'Grants By Country Where Research Was Conducted' map into separate component

--- app/components/GrantsByCountryWhereResearchWasConducted/Card.tsx
+++ app/components/GrantsByCountryWhereResearchWasConducted/Card.tsx
@@ -1,52 +1,22 @@
 import {useState} from 'react'
 import {Flex, Card, Title, Text, Tab, TabList, TabGroup} from "@tremor/react"
 import {ChartBarIcon, GlobeIcon} from "@heroicons/react/solid"
-import {ComposableMap, Geographies, Geography} from 'react-simple-maps'
-import {Tooltip} from 'react-tooltip'
-import {scaleLinear} from "d3-scale"
+import Map from "./Map"
 import MultiSelect from "../MultiSelect"
 import ExportToPngButton from "../ExportToPngButton"
 import {type CardProps} from "../../types/card-props"
 import {filterGrants} from "../../helpers/filter"
 import dataset from '../../../data/dist/filterable-dataset.json'
 import selectOptions from '../../../data/dist/select-options.json'
-import countriesGeoJson from '../../../data/source/geojson/ne_110m_admin_0_countries.json'
 
-export default function GrantsByCountryWhereResearchWasConducted({selectedFilters}: CardProps) {
+export default function GrantsByCountryWhereResearchWasConductedCard({selectedFilters}: CardProps) {
     const [selectedTabIndex, setSelectedTabIndex] = useState<number>(0)
-    const [tooltipContent, setTooltipContent] = useState('')
     const [selectedPathogens, setSelectedPathogens] = useState<string[]>([])
 
     const filteredDataset = filterGrants(
         dataset,
         {...selectedFilters, Pathogen: selectedPathogens},
     )
-
-    const geojson: any = countriesGeoJson
-
-    let filteredCountriesGeoJson = geojson
-
-    filteredCountriesGeoJson.features = filteredCountriesGeoJson.features.map((country: any) => {
-        let properties: any = country.properties
-
-        properties.totalGrants = filteredDataset.filter((grant: any) => grant.ResearchInstitutionCountry === country.properties.ISO_A2).length
-
-        country.properties = properties
-
-        return country
-    })
-
-    const allTotalGrants = filteredCountriesGeoJson
-        .features
-        .filter((country: any) => country.properties.totalGrants)
-        .map((country: any) => country.properties.totalGrants)
-
-    const colorScale = scaleLinear<string>()
-        .domain([
-            Math.min(...allTotalGrants),
-            Math.max(...allTotalGrants),
-        ])
-        .range(["#dbeafe", "#3b82f6"])
 
     return (
         <Card
@@ -83,40 +53,7 @@ export default function GrantsByCountryWhereResearchWasConducted({selectedFilter
                 </Flex>
 
                 {selectedTabIndex === 0 &&
-                    <ComposableMap
-                        projection="geoMercator"
-                        projectionConfig={{
-                            scale: 110,
-                            center: [0, 40],
-                        }}
-                        height={500}
-                    >
-                        <Geographies geography={geojson}>
-                            {({geographies}) =>
-                                geographies.map((geo) => (
-                                    <Geography
-                                        key={geo.rsmKey}
-                                        geography={geo}
-                                        fill={geo.properties.totalGrants ? colorScale(geo.properties.totalGrants) : "#D6D6DA"}
-                                        stroke="#FFFFFF"
-                                        strokeWidth={1}
-                                        onMouseEnter={() => {
-                                            setTooltipContent(`
-                                                <div>
-                                                    <p class="font-bold">${geo.properties.NAME}</p>
-                                                    <p class="text-xs">Grants: ${geo.properties.totalGrants || 0}</p>
-                                                </div>
-                                            `)
-                                        }}
-                                        onMouseLeave={() => {
-                                            setTooltipContent('')
-                                        }}
-                                        data-tooltip-id="country-tooltip"
-                                    />
-                                ))
-                            }
-                        </Geographies>
-                    </ComposableMap>
+                    <Map dataset={filteredDataset} />
                 }
 
                 {selectedTabIndex === 1 &&
@@ -144,18 +81,6 @@ export default function GrantsByCountryWhereResearchWasConducted({selectedFilter
                     filename="grants-by-country-where-research-was-conducted"
                 />
             </Flex>
-
-            <Tooltip
-                id="country-tooltip"
-                float={true}
-                isOpen={tooltipContent !== ''}
-                noArrow={true}
-                place="right-start"
-                offset={10}
-                className="!px-3 !py-2 text-left"
-            >
-                <div dangerouslySetInnerHTML={{__html: tooltipContent}} />
-            </Tooltip>
         </Card>
     )
 }
---
+++ app/components/GrantsByCountryWhereResearchWasConducted/Map.tsx
@@ -0,0 +1,92 @@
+import {useState} from 'react'
+import {ComposableMap, Geographies, Geography} from 'react-simple-maps'
+import {scaleLinear} from "d3-scale"
+import {Tooltip} from 'react-tooltip'
+import countriesGeoJson from '../../../data/source/geojson/ne_110m_admin_0_countries.json'
+
+interface Props {
+    dataset: any[]
+}
+
+export default function Map({dataset}: Props) {
+    const [tooltipContent, setTooltipContent] = useState('')
+
+    const geojson: any = countriesGeoJson
+
+    let filteredCountriesGeoJson = geojson
+
+    filteredCountriesGeoJson.features = filteredCountriesGeoJson.features.map((country: any) => {
+        let properties: any = country.properties
+
+        properties.totalGrants = dataset.filter(
+            (grant: any) => grant.ResearchInstitutionCountry === country.properties.ISO_A2
+        ).length
+
+        country.properties = properties
+
+        return country
+    })
+
+    const allTotalGrants = filteredCountriesGeoJson
+        .features
+        .filter((country: any) => country.properties.totalGrants)
+        .map((country: any) => country.properties.totalGrants)
+
+    const colorScale = scaleLinear<string>()
+        .domain([
+            Math.min(...allTotalGrants),
+            Math.max(...allTotalGrants),
+        ])
+        .range(["#dbeafe", "#3b82f6"])
+
+    return (
+        <div className="w-full h-full">
+            <ComposableMap
+                projection="geoMercator"
+                projectionConfig={{
+                    scale: 110,
+                    center: [0, 40],
+                }}
+                height={500}
+            >
+                <Geographies geography={geojson}>
+                    {({geographies}) =>
+                        geographies.map((geo) => (
+                            <Geography
+                                key={geo.rsmKey}
+                                geography={geo}
+                                fill={geo.properties.totalGrants ? colorScale(geo.properties.totalGrants) : "#D6D6DA"}
+                                stroke="#FFFFFF"
+                                strokeWidth={1}
+                                onMouseEnter={() => {
+                                    setTooltipContent(`
+                                                    <div>
+                                                        <p class="font-bold">${geo.properties.NAME}</p>
+                                                        <p class="text-xs">Grants: ${geo.properties.totalGrants || 0}</p>
+                                                    </div>
+                                                `)
+                                }}
+                                onMouseLeave={() => {
+                                    setTooltipContent('')
+                                }}
+                                data-tooltip-id="country-tooltip"
+                            />
+                        ))
+                    }
+                </Geographies>
+            </ComposableMap>
+
+            <Tooltip
+                id="country-tooltip"
+                float={true}
+                isOpen={tooltipContent !== ''}
+                noArrow={true}
+                place="right-start"
+                offset={10}
+                className="!px-3 !py-2 text-left"
+            >
+                <div dangerouslySetInnerHTML={{__html: tooltipContent}} />
+            </Tooltip>
+        </div >
+    )
+}
