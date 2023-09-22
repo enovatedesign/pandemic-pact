b00f830f5e774d1ca9766935d4a02663224fbb35
Josh Lamb <josh.lamb@enovate.co.uk>
2023-09-22 17:29:38 +0100

    Dummy mobile menu

--- app/components/NavigationMenu.tsx
+++ app/components/NavigationMenu.tsx
@@ -1,25 +1,33 @@
 import Link from 'next/link'
 import {usePathname} from 'next/navigation'
 import {getLinksArray} from '../helpers/nav'
+import {MenuIcon} from '@heroicons/react/solid'
 
 export default function NavigationBar() {
     const pathname = usePathname()
     const links = getLinksArray()
 
     return (
-        <nav className="fixed top-8 right-8 z-30">
-            <ul className="flex space-x-10 px-10 py-3 rounded-full border border-primary/25 inner-glow">
-                {links.map(link => (
-                    <li key={link.url}>
-                        <Link 
-                            href={link.url}
-                            className={`uppercase font-medium tracking-wider transition-colors duration-150 ${pathname === link.url ? 'text-white' : 'text-primary focus:text-white hover:text-white'}`}
-                        >
-                            {link.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <div className="fixed top-8 right-8 z-30 flex items-center rounded-full border border-primary/25 inner-glow">
+            <button className="p-3 lg:hidden">
+                <span className="sr-only">Menu</span>
+                <MenuIcon className="w-8 h-8 fill-white" />
+            </button>
+
+            <nav className="hidden px-10 py-3 lg:block">
+                <ul className="flex space-x-10">
+                    {links.map(link => (
+                        <li key={link.url}>
+                            <Link 
+                                href={link.url}
+                                className={`uppercase font-medium tracking-wider transition-colors duration-150 ${pathname === link.url ? 'text-white' : 'text-primary focus:text-white hover:text-white'}`}
+                            >
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
     )
 }
