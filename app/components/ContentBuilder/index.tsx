1cd7e704e5add8910479bae5872037bf5680e273
Michael Walsh <michael.walsh@enovate.co.uk>
2023-10-24 20:08:20 +0100

    Remove obsolete Matrix blocks

--- app/components/ContentBuilder/index.tsx
+++ app/components/ContentBuilder/index.tsx
@@ -3,14 +3,10 @@ import ButtonBlock from "./Blocks/Button";
 import DividerBlock from "./Blocks/Divider";
 import EmbeddedMediaBlock from "./Blocks/EmbeddedMedia";
 import GalleryBlock from "./Blocks/Gallery";
-import HeadingBlock from "./Blocks/Heading";
 import HighlightedRichTextBlock from "./Blocks/HighlightedRichText";
 import ImageBlock from "./Blocks/Image";
-import LeadTextBlock from "./Blocks/LeadText";
 import MultiColumnsBlock from "./Blocks/MultiColumns";
 import RichTextBlock from "./Blocks/RichText";
-import StatisticsBlock from "./Blocks/Statistics";
-import SubHeadingBlock from "./Blocks/SubHeading";
 import FunderLogoAndStatementBlock from "./Blocks/FunderLogoAndStatement";
 
 const blocks = {
@@ -18,14 +14,10 @@ const blocks = {
 	divider: DividerBlock,
 	embeddedMedia: EmbeddedMediaBlock,
 	gallery: GalleryBlock,
-	heading: HeadingBlock,
 	highlightedRichText: HighlightedRichTextBlock,
 	image: ImageBlock,
-	leadText: LeadTextBlock,
 	multiColumns: MultiColumnsBlock,
 	richText: RichTextBlock,
-	statistics: StatisticsBlock,
-	subHeading: SubHeadingBlock,
 	funderLogoAndStatement: FunderLogoAndStatementBlock,
 };
 
