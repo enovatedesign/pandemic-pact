9d566a6c1e1fb34ef18891d333ff95f8f50c0f4f
Michael Walsh <michael.walsh@enovate.co.uk>
2023-10-24 22:19:44 +0100

    Add inView animation

--- app/components/ContentBuilder/Blocks/Image.tsx
+++ app/components/ContentBuilder/Blocks/Image.tsx
@@ -1,46 +1,77 @@
 import Image from 'next/image';
 import BlockWrapper from '../BlockWrapper';
+import { useInView, animated } from '@react-spring/web';
 
 export default function ImageBlock({ block }) {
+    // console.log(block);
 
-	// console.log(block);
+    const [ref, springs] = useInView(
+        () => ({
+            from: {
+                opacity: 0,
+                y: 100,
+            },
+            to: {
+                opacity: 1,
+                y: 0,
+            },
+        }),
+        {
+            once: true,
+        }
+    );
 
-    const caption = block.caption
-    const image = block.image[0]
-	const width = block.width
+    const caption = block.caption;
+    const image = block.image[0];
+    const width = block.width;
 
-	const imageWidthLookup = {
-		'full': 			{classes: 'w-full', 	sizes: '100vw'},
-		'three-quarters': 	{classes: 'w-3/4', 		sizes: '(min-width: 786px) 66vw, (min-width: 1024px) 75vw, 100vw' },
-		'two-thirds':     	{classes: 'w-2/3', 		sizes: '(min-width: 786px) 66vw, 100vw'},
-    	'one-half': 		{classes: 'w-1/2', 		sizes: '(min-width: 786px) 66vw, (min-width: 1024px) 50vw, 100vw'},
-	}
+    const imageWidthLookup = {
+        full: { 
+			classes: 'w-full', 
+			sizes: '100vw'
+		},
+        'three-quarters': {
+            classes: 'w-3/4',
+            sizes: '(min-width: 786px) 66vw, (min-width: 1024px) 75vw, 100vw',
+        },
+        'two-thirds': {
+            classes: 'w-2/3',
+            sizes: '(min-width: 786px) 66vw, 100vw',
+        },
+        'one-half': {
+            classes: 'w-1/2',
+            sizes: '(min-width: 786px) 66vw, (min-width: 1024px) 50vw, 100vw',
+        },
+    };
 
     if (image && width) {
-
         const blockClasses = ['mx-auto w-full'].join(' ');
 
         return (
-			<BlockWrapper>
-				<figure className={`${imageWidthLookup[width].classes} mx-auto`}>
-					<div className="breakout">
-						<Image
-							alt={image.altText}
-							height={image.height}
-							src={image.url}
-							width={image.width}
-							sizes={imageWidthLookup[width].sizes}
-							className="w-full"
-							loading="lazy"
-							/>
-					</div>
-					{caption && (
-						<figcaption className="mt-4 font-medium text-sm text-gray-600 dark:text-gray-400">
-							{caption}
-						</figcaption>
-					)}
-				</figure>
-			</BlockWrapper>
+            <BlockWrapper>
+                <animated.div ref={ref} style={springs}>
+                    <figure
+                        className={`${imageWidthLookup[width].classes} mx-auto`}
+                    >
+                        <div className="breakout">
+                            <Image
+                                alt={image.altText}
+                                height={image.height}
+                                src={image.url}
+                                width={image.width}
+                                sizes={imageWidthLookup[width].sizes}
+                                className="w-full"
+                                loading="lazy"
+                            />
+                        </div>
+                        {caption && (
+                            <figcaption className="mt-4 font-medium text-sm text-gray-600 dark:text-gray-400">
+                                {caption}
+                            </figcaption>
+                        )}
+                    </figure>
+                </animated.div>
+            </BlockWrapper>
         );
     } else {
         return null;
