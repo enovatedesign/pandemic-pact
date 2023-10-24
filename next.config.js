f7d9c5bd9706946b2d47cbf73a86cb5da0da3a78
Michael Walsh <michael.walsh@enovate.co.uk>
2023-10-24 18:39:46 +0100

    Add CDN hostname to Next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ca1-pnd.edcdn.com',
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
