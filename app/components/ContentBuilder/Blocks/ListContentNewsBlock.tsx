1e09471f07935efa24eeb2d56244456151200b76
Ben Yarrow <ben.yarrow@enovate.co.uk>
2023-10-27 09:47:53 +0100

    GEt news content displaying on list content news

--- app/components/ContentBuilder/Blocks/ListContentNewsBlock.tsx
+++ app/components/ContentBuilder/Blocks/ListContentNewsBlock.tsx
@@ -2,9 +2,9 @@ import BlockWrapper from "../BlockWrapper"
 import Image from "next/image";
 
 const NewsCard = ({entry}) => {
-    const { title, summary } = entry
-
-    console.log(summary)
+    const { title, summary, } = entry
+    const thumbnailImage = entry.thumbnailImage[0] ?? null
+    console.log(thumbnailImage.alt)
     const classes = [
         'flex flex-col overflow-hidden w-full h-full',
         'transition duration-200',
@@ -12,24 +12,42 @@ const NewsCard = ({entry}) => {
         'hover:shadow-xl hover:scale-105'
     ].join(' ')
 
+    const imageClasses = [
+        'block w-full',
+        'bg-gray-100',
+    ].join(' ')
+    
     return (
-        <div key={entry.index} >
-            <a href={entry.url} className={classes}>
-                {/* <Image 
-                    src={thumbnailImage[0].url}
-                /> */}
-                <h3 className='text-xl text-primary'>
-                    {title}
-                </h3>
-                <p>
-                    {summary}
-                </p>
-            </a>
+        <div key={entry.index} className="h-full" >
+            <article className={classes}>
+                <a href={entry.url}>
+                    <Image 
+                        src={thumbnailImage.url}
+                        alt={thumbnailImage.alt}
+                        width={thumbnailImage.width}
+                        height={thumbnailImage.height}
+                        className='w-full h-full'
+                    />
+                </a>
+                <div className='Flex flex-ol gap-6 p-6 w-full h-full'>
+                    {title.length > 0 && (
+                        <h3 className='text-xl text-primary uppercase'>
+                            {title}
+                        </h3>
+                    )}
+                    {summary.length > 0 && (
+                        <p>
+                            {summary}
+                        </p>
+                    )}
+                </div>
+            </article>
         </div>
     )
 };
 
-const ListContentNewsBlock = ({block}) => {
+
+const ListContentNewsBlock = ( {block} ) => {
     
     const customEntries = block.customEntries ?? null
 
