980cbebb471bc82bca3d19f928d39fe539245926
Ben Yarrow <ben.yarrow@enovate.co.uk>
2023-10-27 10:38:16 +0100

    Build News card and seperate into its own component file

--- app/components/ContentBuilder/Blocks/ListContentNewsBlock.tsx
+++ app/components/ContentBuilder/Blocks/ListContentNewsBlock.tsx
@@ -1,50 +1,5 @@
 import BlockWrapper from "../BlockWrapper"
-import Image from "next/image";
-
-const NewsCard = ({entry}) => {
-    const { title, summary, } = entry
-    const thumbnailImage = entry.thumbnailImage[0] ?? null
-    console.log(thumbnailImage.alt)
-    const classes = [
-        'flex flex-col overflow-hidden w-full h-full',
-        'transition duration-200',
-        'rounded bg-white shadow-lg',
-        'hover:shadow-xl hover:scale-105'
-    ].join(' ')
-
-    const imageClasses = [
-        'block w-full',
-        'bg-gray-100',
-    ].join(' ')
-    
-    return (
-        <div key={entry.index} className="h-full" >
-            <article className={classes}>
-                <a href={entry.url}>
-                    <Image 
-                        src={thumbnailImage.url}
-                        alt={thumbnailImage.alt}
-                        width={thumbnailImage.width}
-                        height={thumbnailImage.height}
-                        className='w-full h-full'
-                    />
-                </a>
-                <div className='Flex flex-ol gap-6 p-6 w-full h-full'>
-                    {title.length > 0 && (
-                        <h3 className='text-xl text-primary uppercase'>
-                            {title}
-                        </h3>
-                    )}
-                    {summary.length > 0 && (
-                        <p>
-                            {summary}
-                        </p>
-                    )}
-                </div>
-            </article>
-        </div>
-    )
-};
+import NewsCard from "../News/NewsCard.js"
 
 
 const ListContentNewsBlock = ( {block} ) => {
---
+++ app/components/ContentBuilder/News/NewsCard.js
@@ -0,0 +1,54 @@
+import Image from "next/image";
+
+const NewsCard = ({entry}, {summaryCustomClasses = ''}) => {
+    const { title, summary, } = entry
+    const thumbnailImage = entry.thumbnailImage[0] ?? null
+    
+    const summaryClasses = [
+        summaryCustomClasses
+    ].join(' ')
+    
+    console.log(summaryClasses)
+    const classes = [
+        'flex flex-col overflow-hidden w-full h-full',
+        'transition duration-200',
+        'rounded bg-white shadow-lg',
+        'hover:shadow-xl hover:scale-105'
+    ].join(' ')
+
+    const imageClasses = [
+        'block w-full h-full',
+        'bg-gray-100',
+    ].join(' ')
+    
+    return (
+        <div key={entry.index} className="h-full">
+            <article className={classes}>
+                <a href={entry.url}>
+                    <Image 
+                        src={thumbnailImage.url}
+                        alt={thumbnailImage.alt}
+                        width={thumbnailImage.width}
+                        height={thumbnailImage.height}
+                        className={imageClasses}
+                        loading="lazy"
+                    />
+                </a>
+                <div className='flex flex-col gap-6 p-6 w-full h-full'>
+                    {title.length > 0 && (
+                        <h3 className='text-xl text-primary uppercase'>
+                            {title}
+                        </h3>
+                    )}
+                    {summary.length > 0 && (
+                        <p>
+                            {summary}
+                        </p>
+                    )}
+                </div>
+            </article>
+        </div>
+    )
+};
+
+export default NewsCard
