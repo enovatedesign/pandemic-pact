b4df42aa1644f19a21a0f84ab988a5a27f5c1d9b
Ben Yarrow <ben.yarrow@enovate.co.uk>
2023-10-30 11:23:51 +0000

    Add content to download block

--- app/components/ContentBuilder/Blocks/Download.tsx
+++ app/components/ContentBuilder/Blocks/Download.tsx
@@ -20,45 +20,77 @@ const DownloadBlock = ({ block }: Props) => {
 	
 	const title = customText ? customText :  text
 	
-	
 	const downloadKinds = [
 		{
 			match: '.xlsx',
 			image: '/images/file-types/excel.png',
+			kind: 'Excel Spreadsheet'
 		},
 		{
 			match: '.pdf',
 			image: '/images/file-types/pdf.png',
+			kind: 'PDF'
 		}, 
 		{
 			match: '.pptx',
 			image: '/images/file-types/powerpoint.png',
+			kind: 'PowerPoint Presentation'
 		},
 		{
 			match: '.docx',
 			image: '/images/file-types/word.png',
+			kind: 'Word Document'
 		},
 		{
 			match: '.png',
-			image: '/images/file-types/image.png'
+			image: '/images/file-types/image.png',
+			kind: 'Image'
 		}
 	]	
 	
 
-	const filteredKind = downloadKinds.filter(kind => url.includes(kind.match)).map((filtered) => {return filtered.image}) 
+	const filteredKind = downloadKinds.filter(kind => url.includes(kind.match)) 
 
 	const fallbackImage = "/images/file-types/file.png"
-	const image = filteredKind.length > 0 ? filteredKind[0] : fallbackImage
+	const image = filteredKind.length > 0 ? filteredKind[0].image : fallbackImage
+	const downloadKind = filteredKind[0].kind
 
 	return (
 		<BlockWrapper>
-			<Image
-				src={image}
-				width={100}
-				height={100}
-				alt="Download"
-				className=""
-			/>
+			{url && (
+				<article>
+					<a href={url} target="_blank" className="group relative flex justify-between items-center gap-4 bg-gray-100 lg:pr-24 transition hover:shadow-md hover:scale-[1.02]">
+						<div className="flex flex-row items-center gap-4 p-3">
+							<div className="bg-white rounded p-3">
+								<Image
+									src={image}
+									width={50}
+									height={50}
+									alt="Download"
+									className=""
+								/>
+							</div>
+							<div>
+								{title && (
+									<h2 className="lg:text-xl text-gray-700">
+										{title}
+									</h2>
+								)}
+
+								{downloadKind && (
+									<p className="text-sm lg:text-base mt-1 text-gray-400">
+										Download {downloadKind} <span aria-hidden="true">&bull;</span> 
+									</p>
+								)}
+
+							</div>
+						</div>
+						<div className="hidden lg:flex absolute right-0 top-0 bottom-0 justify-center items-center bg-gray-300 p-5">
+							<svg xmlns="http:www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-current w-6 h-6 text-white transition-colors duration-300 group-hover:text-primary"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zM432 456c-13.3 0-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24z"/></svg>
+						</div>
+					</a>
+				</article>
+			)}
 		</BlockWrapper>
 	);
 }
@@ -66,82 +98,82 @@ const DownloadBlock = ({ block }: Props) => {
 export default DownloadBlock
 
 
-// {# @var block \craft\elements\MatrixBlock #}
+//  {# @var block \craft\elements\MatrixBlock #}
 
-// {% import "_components/macros" as macros %}
+//  {% import "_components/macros" as macros %}
 
-// {% set download = block.download %}
-// {% set customText = download.customText %}
+//  {% set download = block.download %}
+//  {% set customText = download.customText %}
 
-// {% set downloadKinds = {
-//     excel: 'Excel Spreadsheet'|t,
-//     pdf: 'PDF'|t,
-//     powerpoint: 'PowerPoint Presentation'|t,
-//     word: "Word Document"|t,
-//     image: "Image"|t,
-// } %}
+//  {% set downloadKinds = {
+//      excel: 'Excel Spreadsheet'|t,
+//      pdf: 'PDF'|t,
+//      powerpoint: 'PowerPoint Presentation'|t,
+//      word: "Word Document"|t,
+//      image: "Image"|t,
+//  } %}
 
-// {% if download|length %}
+//  {% if download|length %}
 
-//     {% set downloadAsset = download.getElement %}
-//     {% if downloadAsset|length %}
-//         {% set downloadType = download.getElement.className %}
-//     {% endif %}
+//      {% set downloadAsset = download.getElement %}
+//      {% if downloadAsset|length %}
+//          {% set downloadType = download.getElement.className %}
+//      {% endif %}
 
-//     {% if (downloadType is defined) and (downloadType == 'craft\\elements\\Asset') %}
-//         {% set downloadKind = downloadAsset.kind %}
-//         {% set downloadKindText = downloadKinds[downloadKind] %}
-//     {% endif %}
+//      {% if (downloadType is defined) and (downloadType == 'craft\\elements\\Asset') %}
+//          {% set downloadKind = downloadAsset.kind %}
+//          {% set downloadKindText = downloadKinds[downloadKind] %}
+//      {% endif %}
 
-//     {% embed '_components/blocks/blockWrapper' %}
+//      {% embed '_components/blocks/blockWrapper' %}
 
-//         {% block blockContent %}
+//          {% block blockContent %}
 
-//             {% import "_components/macros" as macros %}
+//              {% import "_components/macros" as macros %}
 
-//             <article {{ macros.inView({ customClasses: 'mb-5 last:mb-0' }) }}>
-//                 <a href="{{ downloadAsset.url }}" target="_blank" className="group relative flex justify-between items-center gap-4 bg-gray-100 lg:pr-24 transition hover:shadow-md hover:scale-[1.02]">
-//                     <div className="flex flex-row items-center gap-4 p-3">
-//                         <div className="bg-white rounded p-3">
-//                             {% if downloadKind is defined %}
-//                                 <img src="{{ alias('@interfaceAssetsBaseUrl/dist/images/file-types/' ~ downloadKind ~ '.png') }}"
-//                                         alt="{{ "Download this file"|t }}"
-//                                         width="280"
-//                                         height="323"
-//                                         className="w-8 lg:w-12"
-//                                         crossorigin loading="lazy">
-//                             {% else %}
-//                                 <img src="{{ alias('@interfaceAssetsBaseUrl/dist/images/file-types/file.png') }}"
-//                                         alt="{{ "Download this file"|t }}"
-//                                         width="280"
-//                                         height="323"
-//                                         className="w-8 lg:w-12"
-//                                         crossorigin loading="lazy">
-//                             {% endif %}
-//                         </div>
-//                         <div>
-//                             {% if downloadKindText is defined %}
+//              <article {{ macros.inView({ customClasses: 'mb-5 last:mb-0' }) }}>
+//                  <a href="{{ downloadAsset.url }}" target="_blank" className="group relative flex justify-between items-center gap-4 bg-gray-100 lg:pr-24 transition hover:shadow-md hover:scale-[1.02]">
+//                      <div className="flex flex-row items-center gap-4 p-3">
+//                          <div className="bg-white rounded p-3">
+//                              {% if downloadKind is defined %}
+//                                  <img src="{{ alias('@interfaceAssetsBaseUrl/dist/images/file-types/' ~ downloadKind ~ '.png') }}"
+//                                          alt="{{ "Download this file"|t }}"
+//                                          width="280"
+//                                          height="323"
+//                                          className="w-8 lg:w-12"
+//                                          crossorigin loading="lazy">
+//                              {% else %}
+//                                  <img src="{{ alias('@interfaceAssetsBaseUrl/dist/images/file-types/file.png') }}"
+//                                          alt="{{ "Download this file"|t }}"
+//                                          width="280"
+//                                          height="323"
+//                                          className="w-8 lg:w-12"
+//                                          crossorigin loading="lazy">
+//                              {% endif %}
+//                          </div>
+//                          <div>
+//                              {% if downloadKindText is defined %}
 
-//                                 <h2 className="lg:text-xl text-gray-700">{{ customText|default(downloadAsset.title) }}</h2>
-//                                 <p className="text-sm lg:text-base mt-1 text-gray-400">
-//                                     Download {{ downloadKindText }} <span aria-hidden="true">&bull;</span> {{ downloadAsset.size|filesize }}
-//                                 </p>
+//                                  <h2 className="lg:text-xl text-gray-700">{{ customText|default(downloadAsset.title) }}</h2>
+//                                  <p className="text-sm lg:text-base mt-1 text-gray-400">
+//                                      Download {{ downloadKindText }} <span aria-hidden="true">&bull;</span> {{ downloadAsset.size|filesize }}
+//                                  </p>
 
-//                             {% elseif customText %}
+//                              {% elseif customText %}
 
-//                                 <h2>{{ customText }}</h2>
+//                                  <h2>{{ customText }}</h2>
 
-//                             {% endif %}
-//                         </div>
-//                     </div>
-//                     <div className="hidden lg:flex absolute right-0 top-0 bottom-0 justify-center items-center bg-gray-300 p-5">
-//                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-current w-6 h-6 text-white transition-colors duration-300 group-hover:text-primary"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zM432 456c-13.3 0-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24z"/></svg>
-//                     </div>
-//                 </a>
-//             </article>
+//                              {% endif %}
+//                          </div>
+//                      </div>
+//                      <div className="hidden lg:flex absolute right-0 top-0 bottom-0 justify-center items-center bg-gray-300 p-5">
+//                          <svg xmlns="http:www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-current w-6 h-6 text-white transition-colors duration-300 group-hover:text-primary"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https:fontawesome.com License - https:fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zM432 456c-13.3 0-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24z"/></svg>
+//                      </div>
+//                  </a>
+//              </article>
 
-//         {% endblock %}
+//          {% endblock %}
 
-//     {% endembed %}
+//      {% endembed %}
 
-// {% endif %}
+//  {% endif %}
