928adbe5bddcf17a72936c3cf59be667e5d48e7c
Ben Yarrow <ben.yarrow@enovate.co.uk>
2023-11-02 14:40:55 +0000

    Seperate model from list team members to decrease code in file

--- app/components/ContentBuilder/Blocks/ListTeamMembersBlock.tsx
+++ app/components/ContentBuilder/Blocks/ListTeamMembersBlock.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image"
 import BlockWrapper from "../BlockWrapper"
-import { Fragment, useState } from "react"
-import { Dialog, Transition } from "@headlessui/react"
-import RichText from "../Common/RichText"
+import { useState } from "react"
+import TeamMembersModal from "./Team Members/TeamMembersModal"
 
 type Props = {
     block: {
@@ -44,7 +43,7 @@ const ListTeamMembersBlock = ({block}: Props) => {
                     
                     {customEntries.map((entry, index) => {
                         
-                        const {title, jobTitle, postNominalLetters, aboutText, thumbnailImage} = entry
+                        const {title, jobTitle, thumbnailImage} = entry
 
                         const handleOpen = () => {
                             setActiveIndex(index)
@@ -89,71 +88,7 @@ const ListTeamMembersBlock = ({block}: Props) => {
                                 </div>
                                 
                                 {activeIndex === index && (
-                                    <Transition appear show={isOpen} as={Fragment}>
-                                        <Dialog as="div" className="relative z-10" onClose={handleClose}>
-                                            <Transition.Child
-                                                as={Fragment}
-                                                enter="ease-out duration-300"
-                                                enterFrom="opacity-0"
-                                                enterTo="opacity-100"
-                                                leave="ease-in duration-200"
-                                                leaveFrom="opacity-100"
-                                                leaveTo="opacity-0"
-                                            >
-                                                <div className="fixed inset-0 bg-black/40" />
-                                            </Transition.Child>
-
-                                            <div className="fixed inset-0 container">
-                                                <div className="flex min-h-full items-center justify-center">
-                                                    <Transition.Child
-                                                        as={Fragment}
-                                                        enter="ease-out duration-300"
-                                                        enterFrom="opacity-0 scale-95"
-                                                        enterTo="opacity-100 scale-100"
-                                                        leave="ease-in duration-200"
-                                                        leaveFrom="opacity-100 scale-100"
-                                                        leaveTo="opacity-0 scale-95"
-                                                    >
-                                                        <Dialog.Panel className="grid grid-cols-1 xl:grid-cols-2 gap-8 lg:gap-12 bg-white p-12 rounded-2xl shadow-2xl shadow-black/40">
-
-                                                            <div className="">
-                                                                <Image 
-                                                                    src={thumbnailImage[0].url}
-                                                                    alt={thumbnailImage[0].alt}
-                                                                    width={thumbnailImage[0].width}
-                                                                    height={thumbnailImage[0].height}
-                                                                    className="w-full h-full object-cover rounded-2xl"
-                                                                    loading="lazy"
-                                                                />
-                                                            </div>
-                                                            <div className="flex flex-col justify-between">
-                                                                <div className="flex flex-col space-y-4 xl:space-y-8">
-                                                                    <Dialog.Title
-                                                                        as="h3"
-                                                                        className="text-2xl md:text-3xl leading-6"
-                                                                    >
-                                                                        {title}
-                                                                    </Dialog.Title>
-
-                                                                    <RichText text={aboutText}/>
-                                                                </div>
-
-                                                                <div className="flex justify-end">
-                                                                    <button
-                                                                        type="button"
-                                                                        className="rounded-md border-2 border-primary px-4 py-2 text-sm lg:text-xl text-primary hover:bg-primary hover:text-white transition duration-300"
-                                                                        onClick={handleClose}
-                                                                    >
-                                                                        Close
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        </Dialog.Panel>
-                                                    </Transition.Child>
-                                                </div>
-                                            </div>
-                                        </Dialog>
-                                    </Transition>
+                                    <TeamMembersModal entry={entry} isOpen={isOpen} handleClose={handleClose}/>
                                 )}
                             </li>
                         )
