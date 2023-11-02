c9353dc77a45e68c12556dfa2c5d16d8841bfd0d
Ben Yarrow <ben.yarrow@enovate.co.uk>
2023-11-02 16:58:15 +0000

    Add accordion to grants page abstract content

--- app/grants/[id]/StaticPage.tsx
+++ app/grants/[id]/StaticPage.tsx
@@ -1,6 +1,13 @@
+"use client"
+
+import { useState } from 'react'
+import AnimateHeight from 'react-animate-height';
+import { ChevronDownIcon } from "@heroicons/react/solid"
+
 import Link from 'next/link'
 import {Accordion, AccordionHeader, AccordionBody, AccordionList, Grid, Col, Card, Title, Subtitle, Flex, Text, Metric, List, ListItem} from '@tremor/react'
 import Layout from "../../components/Layout"
+import RichText from '@/app/components/ContentBuilder/Common/RichText'
 
 interface Props {
     grant: any
@@ -71,104 +78,120 @@ export default function StaticPage({grant}: Props) {
         })
     }
 
+    const [abstractShow, setAbstractShow] = useState(false)
+
     return (
-        <Layout title={grant.GrantTitleEng}>
-            <div className="container mx-auto my-6 lg:my-12">
-                <Grid numItemsLg={6} className="gap-6">
-                    <Col
-                        numColSpanLg={4}
-                        className="flex flex-col gap-6"
-                    >
-                        <Card className="grant-abstract">
-                            <Title>Abstract</Title>
+        <div className="bg-gradient-to-b from-primary to-white">
+            <Layout title={grant.GrantTitleEng} >
+                <div className="container mx-auto my-6 lg:my-12 ">
+                    <Grid numItemsLg={1} className="gap-6">
+                        <Col
+                            numColSpanLg={1}
+                            className="flex flex-col gap-6 bg-white p-12 rounded-2xl border-4 border-text-tremor-emphasis"
+                        >
+                            <div className="grant-abstract flex flex-col space-y-4">
+                                <Title className='text-secondary uppercase tracking-widest text-3xl'>Abstract</Title>
+                                <AnimateHeight
+                                    duration={300}
+                                    height={abstractShow ? 'auto' : 100}
+                                    className='relative'
+                                >  
+                                    <RichText text={grant.Abstract} customClasses='min-w-full text-tremor-emphasis tracking-wider'/>
+                                    {!abstractShow && (
+                                        <div className='absolute inset-0 z-10 top-0 left-0  bg-gradient-to-b from-transparent to-white transition duration-300' />
+                                    )}
+                                    </AnimateHeight>
 
-                            <div
-                                className="mt-2"
-                                dangerouslySetInnerHTML={{__html: grant.Abstract}}
-                            />
-                        </Card>
+                                <button onClick={() => setAbstractShow(!abstractShow)} className='w-auto uppercase text-tremor-emphasis tracking wider text-lg flex space-x-2 items-center'>
+                                    <span className='inline-flex '>
+                                        {abstractShow ? "read less" : "read more"}
+                                    </span>
+                                    <ChevronDownIcon className={`${abstractShow && "-rotate-180"} transition duration-300 w-12 h-12`}/>
+                                </button>
+                            </div>
 
-                        {grant.LaySummary &&
-                            <Card className="grant-lay-summary">
-                                <Title>Lay Summary</Title>
+                            {grant.LaySummary &&
+                                <Card className="grant-lay-summary">
+                                    <Title>Lay Summary</Title>
 
-                                <div
-                                    className="mt-2"
-                                    dangerouslySetInnerHTML={{__html: grant.LaySummary}}
-                                />
-                            </Card>
-                        }
-
-                        {grant.PubMedLinks?.length > 0 &&
-                            <Card>
-                                <Title>Publications</Title>
-
-                                <AccordionList className="mt-2 !shadow-none">
-                                    {grant.PubMedLinks.map((link: any, index: number) => (
-                                        <Accordion
-                                            key={index}
-                                            className="border-0 rounded-none"
-                                        >
-                                            <AccordionHeader className="items-start pl-0">
-                                                <p
-                                                    className="text-left"
-                                                    dangerouslySetInnerHTML={{__html: link.title}}
-                                                />
-                                            </AccordionHeader>
-
-                                            <AccordionBody className="flex flex-col pl-0 gap-y-4">
-                                                <div>
-                                                    <Subtitle className="font-bold">Authors</Subtitle>
-                                                    <Text>{link.authorString}</Text>
-                                                </div>
-
-                                                <div>
-                                                    <Subtitle className="font-bold">Publish Year</Subtitle>
-                                                    <Text>{link.pubYear}</Text>
-                                                </div>
-
-                                                {link.journalInfo?.journal?.title &&
-                                                    <div>
-                                                        <Subtitle className="font-bold">Journal</Subtitle>
-                                                        <Text>{link.journalInfo.journal.title}</Text>
-                                                    </div>
-                                                }
-
-                                                <div>
-                                                    <Subtitle className="font-bold">DOI</Subtitle>
-                                                    <Text>{link.doi}</Text>
-                                                </div>
-
-                                                <Link
-                                                    href={`https://europepmc.org/article/${link.source}/${link.pmid}`}
-                                                    className="text-right text-blue-500"
-                                                >
-                                                    View at Europe PMC
-                                                </Link>
-                                            </AccordionBody>
-                                        </Accordion>
-                                    ))}
-                                </AccordionList>
-                            </Card>
-                        }
-                    </Col >
-
-                    <Col numColSpanLg={2}>
-                        <div className="space-y-6">
-                            {sidebarItems.map(({text, metric}, index) => (
-                                <Card key={index}>
-                                    <Flex justifyContent="start" className="space-x-4">
-                                        <div className="">
-                                            <Text>{text}</Text>
-                                            <Metric className="mt-2">{metric}</Metric>
-                                        </div>
-                                    </Flex>
+                                    <div
+                                        className="mt-2"
+                                        dangerouslySetInnerHTML={{__html: grant.LaySummary}}
+                                    />
                                 </Card>
-                            ))}
-                        </div>
-                    </Col>
-                </Grid>
-            </div>
-        </Layout>
+                            }
+
+                            {grant.PubMedLinks?.length > 0 &&
+                                <Card>
+                                    <Title>Publications</Title>
+
+                                    <AccordionList className="mt-2 !shadow-none">
+                                        {grant.PubMedLinks.map((link: any, index: number) => (
+                                            <Accordion
+                                                key={index}
+                                                className="border-0 rounded-none"
+                                            >
+                                                <AccordionHeader className="items-start pl-0">
+                                                    <p
+                                                        className="text-left"
+                                                        dangerouslySetInnerHTML={{__html: link.title}}
+                                                    />
+                                                </AccordionHeader>
+
+                                                <AccordionBody className="flex flex-col pl-0 gap-y-4">
+                                                    <div>
+                                                        <Subtitle className="font-bold">Authors</Subtitle>
+                                                        <Text>{link.authorString}</Text>
+                                                    </div>
+
+                                                    <div>
+                                                        <Subtitle className="font-bold">Publish Year</Subtitle>
+                                                        <Text>{link.pubYear}</Text>
+                                                    </div>
+
+                                                    {link.journalInfo?.journal?.title &&
+                                                        <div>
+                                                            <Subtitle className="font-bold">Journal</Subtitle>
+                                                            <Text>{link.journalInfo.journal.title}</Text>
+                                                        </div>
+                                                    }
+
+                                                    <div>
+                                                        <Subtitle className="font-bold">DOI</Subtitle>
+                                                        <Text>{link.doi}</Text>
+                                                    </div>
+
+                                                    <Link
+                                                        href={`https://europepmc.org/article/${link.source}/${link.pmid}`}
+                                                        className="text-right text-blue-500"
+                                                    >
+                                                        View at Europe PMC
+                                                    </Link>
+                                                </AccordionBody>
+                                            </Accordion>
+                                        ))}
+                                    </AccordionList>
+                                </Card>
+                            }
+                        </Col >
+
+                        {/* <Col numColSpanLg={2}>
+                            <div className="space-y-6">
+                                {sidebarItems.map(({text, metric}, index) => (
+                                    <Card key={index}>
+                                        <Flex justifyContent="start" className="space-x-4">
+                                            <div className="">
+                                                <Text>{text}</Text>
+                                                <Metric className="mt-2">{metric}</Metric>
+                                            </div>
+                                        </Flex>
+                                    </Card>
+                                ))}
+                            </div>
+                        </Col> */}
+                    </Grid>
+                </div>
+            </Layout>
+        </div>
     )
 }
