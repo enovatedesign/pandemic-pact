9c61775d3d2c7975f0c731c4415f9daf4bfbf08f
Michael Walsh <michael.walsh@enovate.co.uk>
2023-11-06 17:23:37 +0000

    Dark mode styling fixes to Meet the team and Publications excerpts

--- app/components/ContentBuilder/Blocks/Publications/PublicationsCard.tsx
+++ app/components/ContentBuilder/Blocks/Publications/PublicationsCard.tsx
@@ -5,7 +5,7 @@ const PublicationsCard = ({entry}) => {
     const {title, summary, thumbnailImage, externalLink} = entry
 
     return (
-        <li className="flex flex-col bg-white border-2 border-gray-200 hover:shadow-lg rounded-2xl overflow-hidden hover:scale-105 transition duration-300 cursor-pointer">
+        <li className="flex flex-col bg-white border-2 border-gray-200 hover:shadow-lg rounded-2xl overflow-hidden hover:scale-105 transition duration-300 cursor-pointer dark:bg-gray-800 dark:border-gray-700">
             <a href={externalLink} target="_blank" rel="nofollow noopener noreferrer">
                 <Image 
                     src={thumbnailImage[0].url}
