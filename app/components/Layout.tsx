6242503262e403942bb54cd43563cf55d0244774
Seb Jones <seb.jones@enovate.co.uk>
2023-11-06 10:54:54 +0000

    Add missing prop

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -16,6 +16,7 @@ type Props = {
     children: React.ReactNode,
     title?: string,
     summary?: string,
+    showSummary?: boolean
 }
 
 const Layout = ({title, summary, showSummary, sidebarContent, children}: Props) => {
@@ -60,7 +61,7 @@ const Layout = ({title, summary, showSummary, sidebarContent, children}: Props) 
                             <animated.div
                                 className={`grow pb-6 px-6 overflow-x-hidden ${sidebarOpen ? 'overflow-y-auto' : 'overflow-y-hidden'}`}
                                 style={widthAnimationProps}
-                                >
+                            >
                                 <animated.div style={opacityAnimationProps}>
                                     {sidebarContent}
                                 </animated.div>
@@ -70,11 +71,11 @@ const Layout = ({title, summary, showSummary, sidebarContent, children}: Props) 
                 }
 
                 <div className="w-full relative">
-                    <Header className="absolute w-full left-0"/>
+                    <Header className="absolute w-full left-0" />
 
                     <main id="content">
 
-                        <article aria-labelledby="page-title">                            
+                        <article aria-labelledby="page-title">
 
                             <div className={`h-[20rem] lg:h-[24rem] masthead-background ${mastheadStyles.background}`}>
 
