45f85055e050b8fc84d5ecebdc28f5d762362ab2
Ben Yarrow <ben.yarrow@enovate.co.uk>
2023-11-07 10:38:08 +0000

    Add animations to funders block based on index

--- app/components/ContentBuilder/Blocks/FunderLogoAndStatement.tsx
+++ app/components/ContentBuilder/Blocks/FunderLogoAndStatement.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import BlockWrapper from '../BlockWrapper'
 import { defaultProseClasses } from '@/app/helpers/prose-classes'
+import { useInView, animated } from '@react-spring/web';
 
 export default function FunderLogoAndStatement({ block }) {
 	const heading = block.heading
@@ -30,8 +31,25 @@ export default function FunderLogoAndStatement({ block }) {
 
                 <div className="flex flex-col gap-6">
                     {funders.map((funder, index) => {
+
+                        const [ref, springs] = useInView(
+                            () => ({
+                                from: {
+                                    opacity: 0,
+                                    x: index % 2 !== 0 ? 100 : -100,
+                                },
+                                to: {
+                                    opacity: 1,
+                                    x: 0,
+                                },
+                            }),
+                            {
+                                once: true,
+                            }
+                        );
+
                         return (
-                            <div className={`${blockClasses} ${index % 2 !== 0 && 'md:flex-row-reverse'}`} key={index}>
+                            <animated.div className={`${blockClasses} ${index % 2 !== 0 && 'md:flex-row-reverse'}`} key={index} ref={ref} style={springs}>
 
                                 <div className={`grid ${funder.funderLogos.length > 1 ? 'grid-cols-2' : 'grid-cols-1'} items-center gap-6 lg:gap-12`}>
 
@@ -58,7 +76,7 @@ export default function FunderLogoAndStatement({ block }) {
                                     </div>
                                 </div>
 
-                            </div>
+                            </animated.div>
                         )
                     })}
                 </div>
