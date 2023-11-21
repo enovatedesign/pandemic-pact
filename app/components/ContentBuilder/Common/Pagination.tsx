e88ad2cbb53d5a1d8a525ccc8ba8acfc0b40e46c
Seb Jones <seb.jones@enovate.co.uk>
2023-11-21 10:47:11 +0000

    Fix typescript errors in Pagination

--- app/components/ContentBuilder/Common/Pagination.tsx
+++ app/components/ContentBuilder/Common/Pagination.tsx
@@ -1,22 +1,22 @@
 "use client"
-import { useState, useEffect } from "react"
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid"
+import {useState, useEffect} from "react"
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/solid"
 
 interface Props {
     postsPerPage: number,
     totalPosts: number,
-    setFirstItemIndex: ((index: number) => number)
-    setLastItemIndex: ((index: number) => number)
+    setFirstItemIndex: ((index: number) => void)
+    setLastItemIndex: ((index: number) => void)
 }
 
 const Pagination = ({
-    postsPerPage, 
-    totalPosts, 
+    postsPerPage,
+    totalPosts,
     setFirstItemIndex,
     setLastItemIndex,
 }: Props) => {
-    
+
     const router = useRouter()
     const pathName = usePathname()
     const params = useSearchParams()
@@ -24,53 +24,53 @@ const Pagination = ({
 
     const [page, setPage] = useState(pageParam ? Number(pageParam) : 1)
     const totalPages = Array.from({length: (Math.ceil(totalPosts / postsPerPage))}, (_, i) => i + 1)
-    const filteredPages = (totalPages.length > 3 && page > 3) ? totalPages.slice(page-3, page+2 ) : totalPages.splice(0,5)
+    const filteredPages = (totalPages.length > 3 && page > 3) ? totalPages.slice(page - 3, page + 2) : totalPages.splice(0, 5)
 
     useEffect(() => {
         const newPageParam = params.get('page')
-        if (!newPageParam && page !== 1 ) {
+        if (!newPageParam && page !== 1) {
             setPage(1)
         } else if (newPageParam && newPageParam !== page.toString()) {
             setPage(Number(newPageParam))
         }
-        
+
         const lastItemIndex = page * postsPerPage
         setFirstItemIndex(lastItemIndex - postsPerPage)
         setLastItemIndex(lastItemIndex)
     }, [page, postsPerPage, setFirstItemIndex, setLastItemIndex, params])
-    
+
     const updateSearchParams = (number: number) => {
         const newParams = new URLSearchParams(params.toString())
         newParams.set('page', number.toString())
-        
+
         const url = number !== 1 ? `${pathName}?${newParams.toString()}` : pathName
         router.push(url)
     }
-    
+
     const updatePage = (number: number) => {
         setPage(number)
         updateSearchParams(number)
     }
-    
+
     const iconClasses = 'w-8 h-8 text-primary transition duration-300 border-2 border-primary rounded-full hover:bg-primary hover:text-white '
-    
+
     const leftFullStopClasses = [
         page >= 4 ? 'block' : 'invisible'
     ].join(' ')
 
     const rightFullStopClasses = [
-        page < totalPages.length -2 ? 'block' : 'invisible'
+        page < totalPages.length - 2 ? 'block' : 'invisible'
     ].join(' ')
 
     return (
         <div className="flex justify-between between pt-6 md:pt-8 xl:pt-20">
-            <button 
+            <button
                 onClick={() => updatePage(page - 1)}
                 disabled={page === 1}
                 className="flex items-center space-x-4 disabled:cursor-not-allowed"
-            >   
+            >
                 <p className={iconClasses}>
-                    <ChevronLeftIcon className='-translate-x-[1px]'/>
+                    <ChevronLeftIcon className='-translate-x-[1px]' />
                 </p>
                 <span className="hidden md:block text-secondary uppercase font-bold">
                     Previous
@@ -84,11 +84,11 @@ const Pagination = ({
                 <div className="hidden md:flex md:space-x-4 ">
                     {filteredPages.map(number => {
                         const activeButtonClasses = [
-                            page === number ? "bg-primary text-white" :  "border-2 border-primary hover:bg-primary text-secondary hover:text-white transition duration-300"
+                            page === number ? "bg-primary text-white" : "border-2 border-primary hover:bg-primary text-secondary hover:text-white transition duration-300"
                         ].join(' ')
 
                         return (
-                            <>  
+                            <>
                                 <button key={number} onClick={() => updatePage(number)} className={`${activeButtonClasses} w-8 rounded-lg flex items-center justify-center aspect-square`}>
                                     {number}
                                 </button>
@@ -100,10 +100,10 @@ const Pagination = ({
                     <span>.</span><span>.</span><span>.</span>
                 </p>
             </div>
-            
 
-            <button 
-                onClick={() => updatePage(page + 1)} 
+
+            <button
+                onClick={() => updatePage(page + 1)}
                 disabled={page === totalPages.length}
                 className="flex items-center space-x-4 disabled:cursor-not-allowed"
             >
@@ -112,7 +112,7 @@ const Pagination = ({
                 </span>
                 <p className={iconClasses}>
                     <ChevronRightIcon className="translate-x-[1px]" />
-                </p>    
+                </p>
             </button>
         </div>
 
