f2fed3da042dc5519a8b516fa656b04e92e2a3f6
Michael Walsh <michael.walsh@enovate.co.uk>
2023-12-01 15:04:16 +0000

    Add rewrites for XML sitemaps from (YE)

--- next.config.js
+++ next.config.js
@@ -16,6 +16,23 @@ const nextConfig = {
         // !! WARN !!
         ignoreBuildErrors: false,
     },
+    async rewrites() {
+        // Attempted to use an env var for the destination below,
+        // but using string concatenation or template literals seems to break the rewrite :-(
+        // The regex below should be able to handle the following URLs:
+        // - sitemaps-1-section-propertiesSales-1-sitemap.xml
+        // - sitemaps-1-section-propertiesSales-1-sitemap.xsl
+        // - sitemap.xml
+        // - sitemap.xsl
+        // - sitemap-empty.xsl
+        // See: https://regex101.com/r/8QUKgO/1
+        return [
+            {
+                source: '/:path([^\/]*sitemap*\.x[ms]l$)',
+                destination: 'https://content.pandemicpact.org/:path',
+            }
+        ]
+    },
 };
 
 module.exports = nextConfig;
