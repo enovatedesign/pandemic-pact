1b6640e3b8163e8e9b07d328ac856a8beb009e48
Seb Jones <seb.jones@enovate.co.uk>
2023-12-02 18:32:58 +0000

    Fix react key warnings

--- app/components/JumpMenu.tsx
+++ app/components/JumpMenu.tsx
@@ -1,25 +1,24 @@
-import { useState, useEffect, useRef } from 'react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
+import {useState, useEffect, useRef} from 'react'
+import {ChevronDownIcon} from '@heroicons/react/solid'
 import Image from "next/image"
 import AnimateHeight from 'react-animate-height';
 
 interface Props {
     dropdownVisible?: boolean,
     cardData: {
-        map: any;
         title?: string,
         image?: {
             url?: string,
-            altText?: string, 
-            height?: number, 
+            altText?: string,
+            height?: number,
             width?: number,
         },
         id?: string,
-    }
+    }[]
 }
 
 export default function JumpMenu({cardData}: Props) {
-    
+
     const [isOpen, setIsOpen] = useState<Boolean>(false)
 
     const dropdown = useRef<HTMLLIElement>(null)
@@ -39,68 +38,69 @@ export default function JumpMenu({cardData}: Props) {
 
     return (
 
-            <div className="">
-                <div className="container mx-auto h-[50px] flex justify-end items-center">
-                    <span className="sr-only">Jump to a visualisation on this page</span>
-                    <div className="relative">
-                        <div className="inline-flex">
-                            <button onClick={() => setIsOpen(true)} className="inline-flex items-center rounded-full bg-primary uppercase py-2 px-4 hover:bg-primary-darker focus:bg-primary-darker focus:outline-none">
-                                <span className="text-sm font-medium">
-                                    Jump to a visualisation collection
-                                </span>
-                                <ChevronDownIcon className="h-5 w-5" aria-hidden="true"/>
-                            </button>
-                        </div>
-                        
-
-                        {cardData && (
-                            <div className="absolute right-0 z-10 mt-2 w-80 lg:w-96 origin-top-right bg-white rounded-b-lg shadow-2xl">
-                                <AnimateHeight
-                                    duration={300}
-                                    height={isOpen ? 'auto' : 0}
-                                >   
-                                    <ul className="grid grid-cols-2 gap-2 p-4">
-
-                                        {cardData.map((card: any, index: number) => {
-
-                                            const {title, image, id} = card
-                                            
-                                            return (
-                                                <>
-                                                        {id && (
-                                                            <li ref={dropdown} key={index} className="hover:bg-primary-lightest transition-colors duration-300 p-2 rounded-lg h-full">
-                                                                <a href={id}>
-                                                                    <button onClick={() => setIsOpen(false)}>
-                                                                        {image && (
-                                                                            <Image 
-                                                                                src={image.url}
-                                                                                alt={image.altText}
-                                                                                width={image.width}
-                                                                                height={image.height}
-                                                                                className="w-full rounded-lg"
-                                                                                loading="lazy"
-                                                                            />   
-                                                                        )}
-                                                                        {title && (
-                                                                            <p className='text-left pt-4'>
-                                                                                {title}
-                                                                            </p>
-                                                                        )}
-                                                                    </button>
-                                                                </a>
-                                                            </li>
-                                                        )}
-                                                </>
-                                            )
-                                        })}
-                                    </ul>
-                                </AnimateHeight>
-                            </div>
-                        )}
-                                
+        <div className="">
+            <div className="container mx-auto h-[50px] flex justify-end items-center">
+                <span className="sr-only">Jump to a visualisation on this page</span>
+                <div className="relative">
+                    <div className="inline-flex">
+                        <button onClick={() => setIsOpen(true)} className="inline-flex items-center rounded-full bg-primary uppercase py-2 px-4 hover:bg-primary-darker focus:bg-primary-darker focus:outline-none">
+                            <span className="text-sm font-medium">
+                                Jump to a visualisation collection
+                            </span>
+                            <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+                        </button>
                     </div>
+
+
+                    {cardData && (
+                        <div className="absolute right-0 z-10 mt-2 w-80 lg:w-96 origin-top-right bg-white rounded-b-lg shadow-2xl">
+                            <AnimateHeight
+                                duration={300}
+                                height={isOpen ? 'auto' : 0}
+                            >
+                                <ul className="grid grid-cols-2 gap-2 p-4">
+
+                                    {cardData.filter(
+                                        (card: any) => card.id
+                                    ).map((card: any, index: number) => {
+                                        const {title, image, id} = card
+
+                                        return (
+                                            <li
+                                                ref={dropdown}
+                                                key={index}
+                                                className="hover:bg-primary-lightest transition-colors duration-300 p-2 rounded-lg h-full"
+                                            >
+                                                <a href={id}>
+                                                    <button onClick={() => setIsOpen(false)}>
+                                                        {image && (
+                                                            <Image
+                                                                src={image.url}
+                                                                alt={image.altText}
+                                                                width={image.width}
+                                                                height={image.height}
+                                                                className="w-full rounded-lg"
+                                                                loading="lazy"
+                                                            />
+                                                        )}
+                                                        {title && (
+                                                            <p className='text-left pt-4'>
+                                                                {title}
+                                                            </p>
+                                                        )}
+                                                    </button>
+                                                </a>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </AnimateHeight>
+                        </div>
+                    )}
+
                 </div>
             </div>
+        </div>
 
     );
 }
