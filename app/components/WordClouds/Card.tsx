56e6a5fb120f85f7cef6b1f024bf12067b2bcbbb
Seb Jones <seb.jones@enovate.co.uk>
2024-01-10 17:24:36 +0000

    WIP: Move word clouds into a single viz card with tabs

---
+++ app/components/WordClouds/Card.tsx
@@ -0,0 +1,37 @@
+import {useContext} from "react"
+import {BeakerIcon, ExclamationCircleIcon} from "@heroicons/react/solid"
+import VisualisationCard from "../VisualisationCard"
+import DiseaseWordCloud from "./Disease"
+import PathogenWordCloud from "./Pathogen"
+import {GlobalFilterContext} from "../../helpers/filter"
+
+export default function WordCloudsCard() {
+    const {grants} = useContext(GlobalFilterContext)
+
+    const tabs = [
+        {
+            tab: {
+                icon: BeakerIcon,
+                label: "Infectious Diseases",
+            },
+            content: <DiseaseWordCloud />,
+        },
+        {
+            tab: {
+                icon: ExclamationCircleIcon,
+                label: "Priority Pathogens",
+            },
+            content: <PathogenWordCloud />,
+        },
+    ]
+
+    return (
+        <VisualisationCard
+            grants={grants}
+            id="word-clouds"
+            title="Word Clouds"
+            footnote="The amount of funding is represented by the size of the word"
+            tabs={tabs}
+        />
+    )
+}
