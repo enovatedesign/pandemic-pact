3dff6697b70829fa02e590f496b0d19428bb06d2
Seb Jones <seb.jones@enovate.co.uk>
2024-02-01 13:11:14 +0000

    Reorganize Advanced Search methods

--- app/components/AdvancedSearch.tsx
+++ app/components/AdvancedSearch.tsx
@@ -4,16 +4,105 @@ import selectOptions from '../../data/dist/select-options.json'
 import {Select, SelectItem, MultiSelect, MultiSelectItem} from "@tremor/react";
 import Button from './Button'
 
-const camelToSentence = (word: string) => {
-    const result = word.replace(/([A-Z])/g, ' $1');
-    return result.charAt(0).toUpperCase() + result.slice(1);
-}
-
 interface Row {
     field: string,
     values: string[],
     logicalAnd: boolean,
     key: number
+}
+
+export default function AdvancedSearch() {
+    const defaultRow: () => Row = () => ({
+        field: 'StudySubject',
+        values: [],
+        logicalAnd: false,
+        key: new Date().getTime(),
+    })
+
+    const [rows, setRows] = useState<Row[]>([defaultRow()])
+    const [globalAnd, setGlobalAnd] = useState(true)
+
+    const globalAndButtonTextClasses = [
+        globalAnd ? 'order-first left-3' : 'order-last right-4'
+    ].join(' ')
+
+    const globalAndButtonDivClasses = [
+        globalAnd ? 'right-1 transition duration-300' : 'right-1 -translate-x-[48px] transition duration-300'
+    ].join(' ')
+
+    const addRow = () => {
+        setRows([...rows, defaultRow()])
+    }
+
+    const paddingClasses = 'md:pr-[100px]'
+
+    return (
+        <section className="p-4">
+            <div className="pb-4 flex flex-col space-y-2 md:space-y-0 md:flex-row md:items-center md:space-x-2  md:pr-[148px]">
+                <p className="text-secondary uppercase text-sm">
+                    Set the global and/or functionality:
+                </p>
+
+                <button
+                    onClick={() => setGlobalAnd(!globalAnd)}
+                    className="h-8 relative flex items-center bg-secondary w-20 rounded-full"
+                >
+                    <div className={`${globalAndButtonDivClasses} w-6 aspect-square bg-primary rounded-full absolute`}></div>
+
+                    <p className={`${globalAndButtonTextClasses} text-primary absolute uppercase text-xs font-bold pr-2`}>
+                        {globalAnd ? 'and' : 'or'}
+                    </p>
+                </button>
+            </div>
+            <div className="flex flex-col gap-2">
+                {rows.map((row: Row, index: number) => {
+                    const removeRow = (index: number) => {
+                        const updatedRows = [...rows]
+                        updatedRows.splice(index, 1)
+                        setRows(updatedRows)
+                    }
+
+                    return (
+                        <div key={row.key} className="relative w-full">
+                            {index > 0 && (
+                                <p className={`${paddingClasses} py-2 text-center text-secondary uppercase text-sm`}>
+                                    {globalAnd ? 'and' : 'or'}
+                                </p>
+                            )}
+
+                            <AdvancedInputRow
+                                row={row}
+                                rows={rows}
+                                setRows={setRows}
+                                index={index}
+                            >
+                                {index > 0 && (
+                                    <button
+                                        className="absolute right-0 translate-x-1/2 flex items-center justify-center bg-secondary rounded-full active:bg-secondary-lighter active:scale-75 transition duration-200"
+                                        onClick={() => removeRow(index)}
+                                    >
+                                        <MinusIcon className="w-6 aspect-square text-primary active:scale-90 transition duration-200" />
+                                    </button>
+                                )}
+                            </AdvancedInputRow>
+                        </div>
+                    )
+                })}
+
+                {rows.length < 6 && (
+                    <div className={`${paddingClasses} flex justify-center`}>
+                        <Button
+                            size="xsmall"
+                            customClasses="mt-3 flex items-center gap-1"
+                            onClick={addRow}
+                        >
+                            Add a row <PlusIcon className="w-5 h-5" aria-hidden="true" />
+                        </Button>
+                    </div>
+                )}
+            </div>
+        </section>
+    )
 }
 
 type AdvancedRowProps = {
@@ -24,7 +113,7 @@ type AdvancedRowProps = {
     index: number,
 }
 
-const AdvancedInputRow = ({children, row, rows, setRows, index}: AdvancedRowProps) => {
+function AdvancedInputRow({children, row, rows, setRows, index}: AdvancedRowProps) {
 
     const [localRow, setLocalRow] = useState<Row>(row)
 
@@ -132,99 +221,7 @@ const AdvancedInputRow = ({children, row, rows, setRows, index}: AdvancedRowProp
     )
 }
 
-const AdvancedSearch = () => {
-
-    const defaultRow: () => Row = () => ({
-        field: 'StudySubject',
-        values: [],
-        logicalAnd: false,
-        key: new Date().getTime(),
-    })
-
-    const [rows, setRows] = useState<Row[]>([defaultRow()])
-    const [globalAnd, setGlobalAnd] = useState(true)
-
-    const globalAndButtonTextClasses = [
-        globalAnd ? 'order-first left-3' : 'order-last right-4'
-    ].join(' ')
-
-    const globalAndButtonDivClasses = [
-        globalAnd ? 'right-1 transition duration-300' : 'right-1 -translate-x-[48px] transition duration-300'
-    ].join(' ')
-
-    const addRow = () => {
-        setRows([...rows, defaultRow()])
-    }
-
-    const paddingClasses = 'md:pr-[100px]'
-
-    return (
-        <section className="p-4">
-            <div className="pb-4 flex flex-col space-y-2 md:space-y-0 md:flex-row md:items-center md:space-x-2  md:pr-[148px]">
-                <p className="text-secondary uppercase text-sm">
-                    Set the global and/or functionality:
-                </p>
-
-                <button
-                    onClick={() => setGlobalAnd(!globalAnd)}
-                    className="h-8 relative flex items-center bg-secondary w-20 rounded-full"
-                >
-                    <div className={`${globalAndButtonDivClasses} w-6 aspect-square bg-primary rounded-full absolute`}></div>
-
-                    <p className={`${globalAndButtonTextClasses} text-primary absolute uppercase text-xs font-bold pr-2`}>
-                        {globalAnd ? 'and' : 'or'}
-                    </p>
-                </button>
-            </div>
-            <div className="flex flex-col gap-2">
-                {rows.map((row: Row, index: number) => {
-                    const removeRow = (index: number) => {
-                        const updatedRows = [...rows]
-                        updatedRows.splice(index, 1)
-                        setRows(updatedRows)
-                    }
-
-                    return (
-                        <div key={row.key} className="relative w-full">
-                            {index > 0 && (
-                                <p className={`${paddingClasses} py-2 text-center text-secondary uppercase text-sm`}>
-                                    {globalAnd ? 'and' : 'or'}
-                                </p>
-                            )}
-
-                            <AdvancedInputRow
-                                row={row}
-                                rows={rows}
-                                setRows={setRows}
-                                index={index}
-                            >
-                                {index > 0 && (
-                                    <button
-                                        className="absolute right-0 translate-x-1/2 flex items-center justify-center bg-secondary rounded-full active:bg-secondary-lighter active:scale-75 transition duration-200"
-                                        onClick={() => removeRow(index)}
-                                    >
-                                        <MinusIcon className="w-6 aspect-square text-primary active:scale-90 transition duration-200" />
-                                    </button>
-                                )}
-                            </AdvancedInputRow>
-                        </div>
-                    )
-                })}
-
-                {rows.length < 6 && (
-                    <div className={`${paddingClasses} flex justify-center`}>
-                        <Button
-                            size="xsmall"
-                            customClasses="mt-3 flex items-center gap-1"
-                            onClick={addRow}
-                        >
-                            Add a row <PlusIcon className="w-5 h-5" aria-hidden="true" />
-                        </Button>
-                    </div>
-                )}
-            </div>
-        </section>
-    )
+function camelToSentence(word: string) {
+    const result = word.replace(/([A-Z])/g, ' $1');
+    return result.charAt(0).toUpperCase() + result.slice(1);
 }
-
-export default AdvancedSearch
