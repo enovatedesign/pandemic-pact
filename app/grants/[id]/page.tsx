4088047c9fe8c14a90a432199ca9733d98c42266
Seb Jones <seb.jones@enovate.co.uk>
2024-02-10 18:32:20 +0000

    Combine grant StaticPage and ClientPage components

--- app/grants/[id]/page.tsx
+++ app/grants/[id]/page.tsx
@@ -1,30 +1,24 @@
-import {Suspense} from 'react'
-import fs from 'fs-extra'
-import {notFound} from 'next/navigation'
-import StaticPage from './StaticPage'
-import ClientPage from './ClientPage'
+import fs from "fs-extra";
+import { notFound } from "next/navigation";
+import StaticPage from "./StaticPage";
 
 export async function generateStaticParams() {
-    return fs.readJsonSync('./data/dist/grants/index.json').map(
-        (grantId: number) => ({id: `${grantId}`})
-    )
+  return fs
+    .readJsonSync("./data/dist/grants/index.json")
+    .map((grantId: number) => ({ id: `${grantId}` }));
 }
 
-export default function Page({params}: {params: {id: string}}) {
-    const path = `./data/dist/grants/${params.id}.json`
+export default function Page({ params }: { params: { id: string } }) {
+  const path = `./data/dist/grants/${params.id}.json`;
 
-    if (!fs.existsSync(path)) {
-        notFound()
-    }
+  if (!fs.existsSync(path)) {
+    notFound();
+  }
 
-    const grant = fs.readJsonSync(path)
+  const grant = fs.readJsonSync(path);
 
-    // Note that the `Suspense` here is to avoid the following error:
-    // https://nextjs.org/docs/messages/deopted-into-client-rendering
+  // Note that the `Suspense` here is to avoid the following error:
+  // https://nextjs.org/docs/messages/deopted-into-client-rendering
 
-    return (
-        <Suspense fallback={<StaticPage grant={grant} />}>
-            <ClientPage grant={grant} />
-        </Suspense>
-    )
+  return <StaticPage grant={grant} />;
 }
