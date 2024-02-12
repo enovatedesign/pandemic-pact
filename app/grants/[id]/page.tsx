b09fa8bec92f75689705159de2d47e1f7946a91f
Seb Jones <seb.jones@enovate.co.uk>
2024-02-12 10:02:55 +0000

    Combine 'view grant' page and StaticPage components

--- app/grants/[id]/page.tsx
+++ app/grants/[id]/page.tsx
@@ -1,24 +1,48 @@
-import fs from "fs-extra";
-import { notFound } from "next/navigation";
-import StaticPage from "./StaticPage";
+import fs from "fs-extra"
+import { notFound } from "next/navigation"
+import Layout from "../../components/Layout"
+import PageTitle from "./PageTitle"
+import Masthead from "./Masthead"
+import BackToGrantSearchLink from "./BackToGrantSearchLink"
+import AbstractAndLaySummary from "./AbstractAndLaySummary"
+import KeyFacts from "./KeyFacts"
+import Publications from "./Publications"
+import "/app/css/components/results-table.css"
+import "/app/css/components/breakout.css"
 
 export async function generateStaticParams() {
-  return fs
-    .readJsonSync("./data/dist/grants/index.json")
-    .map((grantId: number) => ({ id: `${grantId}` }));
+    return fs
+        .readJsonSync("./data/dist/grants/index.json")
+        .map((grantId: number) => ({ id: `${grantId}` }))
 }
 
 export default function Page({ params }: { params: { id: string } }) {
-  const path = `./data/dist/grants/${params.id}.json`;
+    const path = `./data/dist/grants/${params.id}.json`
 
-  if (!fs.existsSync(path)) {
-    notFound();
-  }
+    if (!fs.existsSync(path)) {
+        notFound()
+    }
 
-  const grant = fs.readJsonSync(path);
+    const grant = fs.readJsonSync(path)
 
-  // Note that the `Suspense` here is to avoid the following error:
-  // https://nextjs.org/docs/messages/deopted-into-client-rendering
+    return (
+        <Layout
+            title={<PageTitle grant={grant} />}
+            mastheadContent={<Masthead grant={grant} />}
+        >
+            <div className="container mx-auto my-12 relative">
+                <BackToGrantSearchLink />
 
-  return <StaticPage grant={grant} />;
+                <AbstractAndLaySummary grant={grant} />
+
+                <div className="gap-6">
+                    <div className="flex flex-col gap-6 bg-white p-6 lg:p-12 rounded-2xl border-2 border-gray-200">
+                        <KeyFacts grant={grant} />
+
+                        <Publications grant={grant} />
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
 }
