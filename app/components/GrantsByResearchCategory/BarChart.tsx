204e71f66186ead9a854d67d06cc157693ce5c28
Seb Jones <seb.jones@enovate.co.uk>
2024-03-13 17:32:29 +0000

    Rough implementation of 'view all subcategories' button

--- app/components/GrantsByResearchCategory/BarChart.tsx
+++ app/components/GrantsByResearchCategory/BarChart.tsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import { ArrowLeftIcon } from '@heroicons/react/solid'
 import { GlobalFilterContext } from '../../helpers/filters'
 import {
     GrantAndFinancialCommitmentBarList,
@@ -19,6 +20,31 @@ interface Props {
 }
 
 export default function BarChart({ chartData }: Props) {
+    const [showAllSubCategories, setShowAllSubCategories] = useState(true)
+
+    if (showAllSubCategories) {
+        return (
+            <>
+                <div className="flex justify-center items-center w-full">
+                    <button
+                        onClick={() => setShowAllSubCategories(false)}
+                        className="flex items-center"
+                    >
+                        <span className="cursor-pointer mr-4 bg-brand-grey-200 p-1.5 rounded-md shadow-lg">
+                            <ArrowLeftIcon className="size-6 text-brand-grey-500" />
+                        </span>
+                    </button>
+
+                    <p className="text-brand-grey-500">
+                        Viewing All Sub-Categories
+                    </p>
+                </div>
+
+                <AllSubCategoriesBarChart chartData={chartData} />
+            </>
+        )
+    }
+
     const subCharts = Object.fromEntries(
         chartData.map(data => {
             return [
@@ -32,11 +58,76 @@ export default function BarChart({ chartData }: Props) {
     )
 
     return (
+        <>
+            <GrantAndFinancialCommitmentBarList
+                data={chartData}
+                brightColours={researchCategoryColours}
+                dimColours={researchCategoryDimColours}
+                subCharts={subCharts}
+            />
+
+            <div className="flex justify-end w-full">
+                <button
+                    className="self-start text-center font-medium rounded-full no-underline transition-colors duration-200 ease-in-out disabled:bg-disabled disabled:cursor-default disabled:hover:bg-disabled px-3 text-sm bg-primary-lightest text-secondary hover:bg-primary-lighter"
+                    onClick={() => setShowAllSubCategories(true)}
+                >
+                    View all sub-categories
+                </button>
+            </div>
+        </>
+    )
+}
+
+function AllSubCategoriesBarChart({ chartData }: Props) {
+    const { grants } = useContext(GlobalFilterContext)
+
+    const subCategoryChartData = chartData.flatMap(
+        ({ 'Category Value': researchCategory }) =>
+            selectOptions.ResearchSubcat.filter(
+                ({ parent }: { parent: string }) => parent === researchCategory
+            ).map(function (researchSubCategory) {
+                const grantsWithKnownAmounts = grants
+                    .filter((grant: any) =>
+                        grant.ResearchSubcat.includes(researchSubCategory.value)
+                    )
+                    .filter(
+                        (grant: any) =>
+                            typeof grant.GrantAmountConverted === 'number'
+                    )
+
+                const grantsWithUnspecifiedAmounts = grants
+                    .filter((grant: any) =>
+                        grant.ResearchSubcat.includes(researchSubCategory.value)
+                    )
+                    .filter(
+                        (grant: any) =>
+                            typeof grant.GrantAmountConverted !== 'number'
+                    )
+
+                const moneyCommitted = grantsWithKnownAmounts.reduce(
+                    ...sumNumericGrantAmounts
+                )
+
+                return {
+                    'Category Value': researchSubCategory.value,
+                    'Category Label': researchSubCategory.label,
+                    'Grants With Known Financial Commitments':
+                        grantsWithKnownAmounts.length,
+                    'Grants With Unspecified Financial Commitments':
+                        grantsWithUnspecifiedAmounts.length,
+                    'Total Grants':
+                        grantsWithKnownAmounts.length +
+                        grantsWithUnspecifiedAmounts.length,
+                    'Known Financial Commitments (USD)': moneyCommitted,
+                }
+            })
+    )
+
+    return (
         <GrantAndFinancialCommitmentBarList
-            data={chartData}
-            brightColours={researchCategoryColours}
-            dimColours={researchCategoryDimColours}
-            subCharts={subCharts}
+            data={subCategoryChartData}
+            brightColours={researchSubCategoryColours}
+            dimColours={researchSubCategoryDimColours}
         />
     )
 }
