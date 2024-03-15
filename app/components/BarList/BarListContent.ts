498909d5fc1c29b68acc081e2ea7610b21a0e7b5
Seb Jones <seb.jones@enovate.co.uk>
2024-03-15 10:14:31 +0000

    Remove unneeded file

--- app/components/BarList/BarListContent.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-export const BarListContext = createContext<{
-    filters: Filters
-    grants: any[]
-}>({
-    filters: emptyFilters(),
-    grants: [],
-})
