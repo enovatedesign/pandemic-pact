07d496aa9e1c81daea95bb24fd97832be877f6d5
Ben Yarrow <ben.yarrow@enovate.co.uk>
2024-03-26 15:14:56 +0000

    Resolve "Key facts border misalignment on mobile"

---
+++ app/helpers/default-meta-data.ts
@@ -0,0 +1,4 @@
+export const defaultMetaData = {
+    title: 'Pandemic PACT Tracker', 
+    description: 'Pandemic PACT tracks and analyses global funding and evidence for research on diseases with pandemic potential and broader research preparedness efforts, ready to pivot in response to outbreaks.',
+}
