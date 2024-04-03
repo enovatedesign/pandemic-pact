5e15088182fc881ca81c3fbde77bc9cd65cd3802
Ben Yarrow <ben.yarrow@enovate.co.uk>
2024-04-03 14:29:48 +0100

    Update styling to allow image export formatting fix

--- app/components/Legend.tsx
+++ app/components/Legend.tsx
@@ -13,18 +13,20 @@ export default function Legend({categories, colours, customWrapperClasses, custo
             {categories.map((category, index) => (
                 <li
                     key={`legend-item-${category}-${index}`}
-                    className="image-legend-list-item group inline-flex items-start truncate px-2 py-0.5 transition cursor-default text-gray-500"
+                    className="image-legend-list-item group inline-flex items-center truncate px-2 py-0.5 transition cursor-default text-gray-500"
                 >
-                    <svg
-                        className="flex-none text-blue-500 h-2 w-2 mr-1.5 opacity-100 mt-[5px]"
-                        fill={colours[index]}
-                        viewBox="0 0 8 8"
-                    >
-                        <circle cx="4" cy="4" r="4"></circle>
-                    </svg>
+                    <div className="image-legend-svg-wrapper">
+                        <svg
+                            className="image-legend-svg-element flex-none text-blue-500 w-2 h-2 mr-1.5 opacity-100"
+                            fill={colours[index]}
+                            viewBox="0 0 8 8"
+                        >
+                            <circle cx="4" cy="4" r="4"></circle>
+                        </svg>
+                    </div>
 
                     <p
-                        className={`${customTextClasses ? customTextClasses : "whitespace-nowrap truncate"} text-sm text-gray-500 opacity-100"`}
+                        className={`${customTextClasses ? customTextClasses : "whitespace-nowrap truncate"} text-sm text-gray-500 opacity-100`}
                     >
                         {category}
                     </p>
