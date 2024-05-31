1345a4dbb29c1f893e951c420480ebcf5fca9eb7
Seb Jones <seb.jones@enovate.co.uk>
2024-05-31 13:18:17 +0100

    Use mapshaper to prepare separate geojson file for WHO regions

--- scripts/generate/prepare-map.ts
+++ scripts/generate/prepare-map.ts
@@ -1,4 +1,5 @@
 import fs from 'fs-extra'
+import { execSync } from 'child_process'
 import _ from 'lodash'
 
 export default async function () {
@@ -6,22 +7,44 @@ export default async function () {
         './data/source/geojson/ne_110m_admin_0_countries.json'
     )
 
-    const outputGeojson = _.pick(inputGeojson, ['type', 'features', 'bbox'])
+    const regionToCountryMapping: { [key: string]: string[] } = fs.readJsonSync(
+        './data/source/region-to-country-mapping.json'
+    )
 
-    outputGeojson.features = outputGeojson.features.map(
+    const countriesGeojson = _.pick(inputGeojson, ['type', 'features', 'bbox'])
+
+    countriesGeojson.features = countriesGeojson.features.map(
         (feature: {
             type: string
             properties: { NAME: string; ISO_N3_EH: string }
         }) => {
+            const name = feature.properties.NAME
+
+            const iso_code = feature.properties.ISO_N3_EH.replace(/^0+/, '')
+
+            const who_region = Object.entries(regionToCountryMapping).find(
+                ([_, countries]) => countries.includes(iso_code)
+            )?.[0]
+
             return {
                 ...feature,
                 properties: {
-                    name: feature.properties.NAME,
-                    iso_code: feature.properties.ISO_N3_EH.replace(/^0+/, ''),
+                    name,
+                    iso_code,
+                    who_region,
                 },
             }
         }
     )
 
-    fs.writeJsonSync('./public/data/world-geo.json', outputGeojson)
+    fs.ensureDirSync('./public/data/geojson')
+
+    fs.writeJsonSync('./public/data/geojson/countries.json', countriesGeojson)
+
+    execSync(`
+        npx mapshaper ./public/data/geojson/countries.json \
+            -dissolve who_region copy-fields=who_region \
+            -clean \
+            -o fix-geometry gj2008 format=geojson ./public/data/geojson/who-regions.json
+    `)
 }
