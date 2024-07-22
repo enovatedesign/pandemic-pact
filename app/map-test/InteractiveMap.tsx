2d716d9e8102c68822c470ff92e867ac77869fe1
Seb Jones <seb.jones@enovate.co.uk>
2024-07-22 11:45:28 +0100

    Revert "Use orthographic projection with custom modelMatrix since we can't use naturalEarth1"
    This reverts commit 7ed7775b440e0e91d37750e429f0baeed40e64f0.

--- app/map-test/InteractiveMap.tsx
+++ app/map-test/InteractiveMap.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { useCallback, useMemo } from 'react'
 import DeckGL from '@deck.gl/react'
 import { GeoJsonLayer } from '@deck.gl/layers'
-import { LinearInterpolator, OrthographicView } from '@deck.gl/core'
 import type { PickingInfo } from '@deck.gl/core'
 
 interface Props {
@@ -12,57 +10,29 @@ interface Props {
 }
 
 export default function InteractiveMap({ geojson, setSelectedCountry }: Props) {
-    const onClick = useCallback(
-        (info: PickingInfo) => {
+    const layer = new GeoJsonLayer<any>({
+        id: 'GeoJsonLayer',
+        data: geojson,
+
+        filled: true,
+        getFillColor: (f: any) => f.properties.colour,
+
+        stroked: true,
+        getLineColor: [255, 255, 255],
+        getLineWidth: 2,
+        lineWidthUnits: 'pixels',
+        pickable: true,
+        onClick: (info: PickingInfo) => {
             setSelectedCountry(info.object?.properties.id ?? null)
         },
-        [setSelectedCountry],
-    )
-
-    const layer = useMemo(
-        () =>
-            new GeoJsonLayer<any>({
-                id: 'GeoJsonLayer',
-                data: geojson,
-
-                filled: true,
-                getFillColor: (f: any) => f.properties.colour,
-
-                stroked: true,
-                getLineColor: [255, 255, 255],
-                getLineWidth: 2,
-                lineWidthUnits: 'pixels',
-                pickable: true,
-                modelMatrix: [1, 0, 0, 0, 0, 1.2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                onClick,
-            }),
-        [geojson, setSelectedCountry],
-    )
-
-    const view = useMemo(
-        () =>
-            new OrthographicView({
-                id: 'orthographic-view',
-                width: 2000,
-                height: 2000,
-                flipY: false,
-                controller: true,
-            }),
-        [],
-    )
-
-    const interpolator = useMemo(
-        () => new LinearInterpolator(['target', 'zoom']),
-        [],
-    )
+    })
 
     return (
         <DeckGL
-            views={view}
             initialViewState={{
-                zoom: 2,
-                transitionInterpolator: interpolator,
-                target: [0, 0, 0],
+                longitude: 0,
+                latitude: 0,
+                zoom: 1,
             }}
             controller={{
                 dragRotate: false,
