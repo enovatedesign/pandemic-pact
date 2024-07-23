c097db9108ab1e043ddfc7a66faa1af430c9f37d
Seb Jones <seb.jones@enovate.co.uk>
2024-07-23 13:30:24 +0100

    Update Mpox Viz Card title and subtitle

--- app/components/GrantsByMpoxResearchPriority.tsx
+++ app/components/GrantsByMpoxResearchPriority.tsx
@@ -2,10 +2,29 @@ import CategoryAndSubcategoryBarList from './CategoryAndSubcategoryBarList/Categ
 import VisualisationCard from './VisualisationCard'
 
 export default function GrantsPerMpoxResearchPriority() {
+    const subtitle = (
+        <>
+            Research priorities as outlined in the{' '}
+            <a
+                href="https://africacdc.org/news-item/communique-united-in-the-fight-against-mpox-in-africa-high-level-emergency-regional-meeting/"
+                className="text-auto"
+            >
+                African Ministers of Health communique
+            </a>{' '}
+            and the{' '}
+            <a href="https://www.who.int/publications/i/item/9789240092907">
+                WHO’s strategic framework for enhancing prevention and control
+                of mpox – 2024-2027
+            </a>
+            .
+        </>
+    )
+
     return (
         <VisualisationCard
             id="grants-by-mpox-research-priority"
-            title="Grants by Mpox Research Priority"
+            title="Grants by Mpox Outbreak Specific Research Priorities"
+            subtitle={subtitle}
         >
             <CategoryAndSubcategoryBarList
                 topOfCardId="mpox-research-priorities"
