70c6f14baf2f7eb35ff9a2e1f65ebab85076f185
Seb Jones <seb.jones@enovate.co.uk>
2024-07-30 16:27:52 +0100

    Move OrthographicView and LinearInterpolator constructors back into useMemos

--- app/components/GrantsByCountryWhereResearchWasConducted/Map/InteractiveMap.tsx
+++ app/components/GrantsByCountryWhereResearchWasConducted/Map/InteractiveMap.tsx
@@ -18,24 +18,6 @@ export default function InteractiveMap({ geojson, onClick }: Props) {
         [onClick],
     )
 
-    const layer = useMemo(
-        () =>
-            new GeoJsonLayer<any>({
-                id: 'GeoJsonLayer',
-                data: geojson,
-                filled: true,
-                getFillColor: (feature: any) => feature.properties.fillColour,
-                stroked: true,
-                getLineColor: [255, 255, 255],
-                lineWidthScale: 1.25,
-                lineWidthUnits: 'pixels',
-                pickable: true,
-                modelMatrix: [1, 0, 0, 0, 0, 1.2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                onClick: onLayerClick,
-            }),
-        [geojson, onLayerClick],
-    )
-
     const getCursor = useCallback(
         ({
             isDragging,
@@ -55,24 +37,48 @@ export default function InteractiveMap({ geojson, onClick }: Props) {
         [],
     )
 
+    const layer = useMemo(
+        () =>
+            new GeoJsonLayer<any>({
+                id: 'GeoJsonLayer',
+                data: geojson,
+                filled: true,
+                getFillColor: (feature: any) => feature.properties.fillColour,
+                stroked: true,
+                getLineColor: [255, 255, 255],
+                lineWidthScale: 1.25,
+                lineWidthUnits: 'pixels',
+                pickable: true,
+                modelMatrix: [1, 0, 0, 0, 0, 1.2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+                onClick: onLayerClick,
+            }),
+        [geojson, onLayerClick],
+    )
+
+    const view = useMemo(
+        () =>
+            new OrthographicView({
+                id: 'orthographic-view',
+                flipY: false,
+                controller: true,
+            }),
+        [],
+    )
+
+    const transitionInterpolator = useMemo(
+        () => new LinearInterpolator(['target', 'zoom']),
+        [],
+    )
+
     return (
         <DeckGL
             width="100%"
             height={450}
             style={{ position: 'relative' }}
-            views={
-                new OrthographicView({
-                    id: 'orthographic-view',
-                    flipY: false,
-                    controller: true,
-                })
-            }
+            views={view}
             initialViewState={{
                 zoom: 1.25,
-                transitionInterpolator: new LinearInterpolator([
-                    'target',
-                    'zoom',
-                ]),
+                transitionInterpolator,
                 target: [0, 15, 0],
             }}
             controller={{
