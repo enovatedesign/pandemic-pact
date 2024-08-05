b9d957a0f9efb184d2478f23ea9cc6671cf32b50
Ben Yarrow <ben.yarrow@enovate.co.uk>
2024-08-05 14:07:07 +0100

    Resolve "Add new Jump Cards block"

---
+++ app/components/ContentBuilder/Blocks/JumpCards.tsx
@@ -0,0 +1,85 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import BlockWrapper from "../BlockWrapper"
+import Card from "../Common/Card"
+import { ChevronDownIcon } from "@heroicons/react/outline"
+import { formatId } from "@/app/helpers/value-formatters"
+
+interface JumpCardProps {
+    block: {
+        jumpCards: {
+            heading: string
+            summary: string
+            image: {
+                width: number
+                height: number
+                url: string
+                altText: string
+            }[]
+            jumpCardId: string
+        }[]
+    }
+}
+
+const JumpCardsBlock = ({block}: JumpCardProps) => {
+
+    const jumpCards = block.jumpCards
+    
+    const [availableElements, setAvailableElements] = useState<Record<string, HTMLElement>>({})
+
+    useEffect(() => {
+        const elementsObject: Record<string, HTMLElement> = {};
+
+        jumpCards.forEach(card => {
+            const formattedId = formatId(card.jumpCardId)
+            const element = document.getElementById(formattedId)
+            
+            if (!element) {
+                console.warn(`Element ID: ${formattedId}, does not exist within the page.`);
+                return 
+            } else if (formattedId in elementsObject) {
+                console.warn(`Duplicate ID found: ${formattedId}, jump card destination IDs must be unique within the context of the current page.`)
+                return
+            } else {
+                elementsObject[formattedId] = element
+            }
+        })
+
+        setAvailableElements(elementsObject)
+    }, [jumpCards])
+
+    return (
+            <BlockWrapper options={{ fill: true, bgColourClass: '' }}>
+                <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5">
+                    {jumpCards.map((card, index: number) => {
+                        
+                        const {heading, summary, jumpCardId} = card
+
+                        const image = card.image[0] ?? null
+                        
+                        const entry = {
+                            url: `#${formatId(jumpCardId)}`,
+                            title: heading,
+                            summary: summary
+                        }
+
+                        return (
+                            <li key={index}>
+                                {availableElements[formatId(jumpCardId)] && (
+                                    <Card
+                                        entry={entry}
+                                        tags={false}
+                                        image={image}
+                                        animatedIcon={<ChevronDownIcon className="w-6 h-6"/>}
+                                    />
+                                )}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </BlockWrapper>
+    )
+}
+
+export default JumpCardsBlock
