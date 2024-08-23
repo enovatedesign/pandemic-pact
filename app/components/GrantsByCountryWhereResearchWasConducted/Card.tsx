a742f629b1cd23af02c6e20f9475507d0a0981a4
Michael Walsh <michael.walsh@enovate.co.uk>
2024-08-23 14:12:16 +0100

    Add instructions to subtitle to map visualisation card

--- app/components/GrantsByCountryWhereResearchWasConducted/Card.tsx
+++ app/components/GrantsByCountryWhereResearchWasConducted/Card.tsx
@@ -25,7 +25,7 @@ export default function GrantsByCountryWhereResearchWasConductedCard() {
         <VisualisationCard
             id="grants-by-country-where-research-was-conducted"
             title="Global Map of Geographical Distribution of Funding Organisations OR Research Locations"
-            subtitle="The information on the research location was collected where available from the grant application, and can be different to the location of research institution."
+            subtitle="The information on the research location was collected where available from the grant application, and can be different to the location of research institution. Click on a country to see country-specific grant information (including joint-funded grants)."
             footnote="Please note: Funding amounts are included only when they have been published by the funder. Some research projects are undertaken in multiple locations (countries) and some are funded by multiple funders. Where research location is not explicitly specified the default used is the location of the research institution receiving the funds."
             tabs={tabs}
         />
