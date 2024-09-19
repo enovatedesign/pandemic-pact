ad65fdbd4347b0fc09fe70844ac6c0b7139a26e7
Ben Yarrow <ben.yarrow@enovate.co.uk>
2024-09-19 14:13:07 +0100

    Adjust map footnote text and footnote props

--- app/components/GrantsByCountryWhereResearchWasConducted/Card.tsx
+++ app/components/GrantsByCountryWhereResearchWasConducted/Card.tsx
@@ -32,7 +32,11 @@ export default function GrantsByCountryWhereResearchWasConductedCard() {
                 id="grants-by-country-where-research-was-conducted"
                 title="Global Map of Geographical Distribution of Funding Organisations OR Research Locations"
                 subtitle="The information on the research location was collected where available from the grant application, and can be different to the location of research institution. Click on a country to see country-specific grant information (including joint-funded grants)."
-                footnote="Please note: Funding amounts are included only when they have been published by the funder. Some research projects are undertaken in multiple locations (countries) and some are funded by multiple funders. Where research location is not explicitly specified the default used is the location of the research institution receiving the funds."
+                footnote={
+                    <p>
+                        Please note: Funding amounts are included only when they have been published by the funder. Some research projects are undertaken in multiple locations &#40;countries&#41;. Some are funded by multiple funders, <strong>the breakdown of joint-funded projects can be found when selecting a country and &#39;show joint-funded countries&#39;.</strong> Where research location is not explicitly specified the default used is the location of the research institution receiving the funds. 
+                    </p>
+                }
                 tabs={tabs}
             />
         </DeckGLRefContext.Provider>
--- app/components/VisualisationCard.tsx
+++ app/components/VisualisationCard.tsx
@@ -17,7 +17,7 @@ interface Props {
     title: string
     subtitle?: string | ReactNode
     chartInstructions?: string
-    footnote?: string
+    footnote?: string | ReactNode
     infoModalContents?: ReactNode
     children?: ReactNode
     tabs?: Array<{
