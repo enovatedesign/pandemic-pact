b51c3f57a41bce3695e5f961f3ea782390670fa9
Ben Yarrow <ben.yarrow@enovate.co.uk>
2025-02-14 10:00:43 +0000

    Resolve "Generate an XML sitemap including entries from Craft CMS"

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,6 @@
 import { MetadataRoute } from 'next'
 import GraphQL from './lib/GraphQl'
-import fs from 'fs';
-import path from 'path';
+import grants from '../public/data/grants.json'
 
 const siteUrl = 'https://www.pandemicpact.org'
 
@@ -9,6 +8,10 @@ interface craftEntryProps {
     uri: string, 
     typeHandle: string
     dateUpdated: string
+}
+
+interface Grant {
+    "GrantID": string
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
@@ -54,33 +57,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         }
     ))
 
-    const grantDirectory = path.join(process.cwd(), 'data', 'dist', 'grants');
-    
-    const readJsonFiles = async () => {
-        try {
-            const grantFileNames = fs.readdirSync(grantDirectory);
-        
-            const grantData = [];
-        
-            for (const grantFileName of grantFileNames) {
-                const grantFilepath = path.join(grantDirectory, grantFileName);
-                const grantContent = fs.readFileSync(grantFilepath, 'utf-8');
-                const grantJsonData = JSON.parse(grantContent);
-                grantData.push(grantJsonData);
-            }
-        
-            return grantData;
-        } catch (error) {
-            console.error('Error reading JSON files:', error);
-            return [];
-        }
-    };
-
-    const grantJsonDataArray = await readJsonFiles();
-
-    const grantPageSitemapData = grantJsonDataArray
-        .filter(({GrantID}) => 
-            GrantID !== undefined
+    const grantPageSitemapData = (grants as Grant[])
+        .filter((grant: Grant) => 
+            grant["GrantID"] !== undefined
         ).map(({ GrantID }: { GrantID: string }) => ({
             url: `${siteUrl}/grants/${GrantID}`
         }))
