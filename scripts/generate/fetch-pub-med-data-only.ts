0064adabc5b47ce85603a4f4b637577d4af3b875
Josh Lamb <jmw.lamb@gmail.com>
2026-01-02 15:09:06 +0000

    Ensure env is loaded so blob token can be read

--- scripts/generate/fetch-pub-med-data-only.ts
+++ scripts/generate/fetch-pub-med-data-only.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv'
 import downloadAndParseDataSheet from './download-and-parse-data-sheets'
 import prepareGrants from './prepare-grants'
 import fetchPubMedData from './fetch-pub-med-data'
 
 (async () => {
+    dotenv.config({ path: './.env.local' })
+    
     await downloadAndParseDataSheet(true)
 
     await prepareGrants()
