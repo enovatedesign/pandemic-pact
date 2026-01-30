dd217649acfd0014521a5edf39a85daa430bc943
Josh Lamb <jmw.lamb@gmail.com>
2026-01-30 10:59:17 +0000

    Prepare search indexes if cached files used

--- scripts/generate/index.ts
+++ scripts/generate/index.ts
@@ -33,6 +33,9 @@ async function main() {
     // OpenSearch will be skipped if SKIP_OPENSEARCH_INDEXING is set
     if (useCachedFiles) {
         info('Using cached static files - skipping grant processing and file generation')
+
+        await prepareSearch()
+
         info('Build complete using cached files')
         return
     }
