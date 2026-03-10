b5f3c3c30f583b98bc14176a56b010a7f6a50bd5
Josh Lamb <jmw.lamb@gmail.com>
2026-03-10 12:45:31 +0000

    Corrects the order of parameters to ensure the previewToken is in the correct order

--- app/lib/EntryTypes/query/NewsArticleQuery.ts
+++ app/lib/EntryTypes/query/NewsArticleQuery.ts
@@ -1,7 +1,7 @@
 import GraphQL from '../../GraphQl'
 import {contentBuilderQuery, seomaticQuery} from '../../Queries'
 
-export default async function NewsArticleQuery(uri: string, previewToken?: string) {
+export default async function NewsArticleQuery(uri: string, entryType: string, sectionHandle: string, previewToken?: string) {
 
     const data = await GraphQL(
         `
